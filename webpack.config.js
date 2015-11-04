0a6dd9b406825b88662c87ec02ef25d5087d38a2
Hu Songtao <idisblueflash@gmail.com>
2015-11-04 11:02:16 +0800

    配置dev模式

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 var ROOT_PATH = path.resolve(__dirname);
 
@@ -9,7 +10,14 @@ module.exports = {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
   },
+  devServer: {
+    histroyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: 'YAOPAI Mobile'
     })
