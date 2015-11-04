8be91334ecb1c07a346a207128f9bc3d7dbd4067
Hu Songtao <idisblueflash@gmail.com>
2015-11-04 09:34:12 +0800

    准备测试app代码

---
+++ app/component.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+  var element = document.createElement('h1');
+
+  element.innerHTML = 'Hello YAOPAI Mobile';
+
+  return element;
+};
---
+++ app/index.js
@@ -0,0 +1,6 @@
+var component = require('./component');
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
