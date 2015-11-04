5c6ccd449bb7ff9db3d67e31aab6836e9d712930
Hu Songtao <idisblueflash@gmail.com>
2015-11-04 12:37:58 +0800

    加入css支持和自动刷新

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,15 @@ module.exports = {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: path.resolve(ROOT_PATH, 'app')
+      }
+    ]
+  },
   devServer: {
     histroyApiFallback: true,
     hot: true,
