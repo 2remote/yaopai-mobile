6d70c1d9234b7157c2583f3128448bd8feb20eb7
Hu Songtao <idisblueflash@gmail.com>
2015-11-09 21:52:43 +0800

    配置静态文件服务器

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('build'));
+app.use('/imgs', express.static('app/imgs'));
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+var server = app.listen(5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
