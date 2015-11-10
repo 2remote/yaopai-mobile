2f0deda66be4a448a9fc94f54bc1889050e8d723
Hu Songtao <idisblueflash@gmail.com>
2015-11-10 19:24:03 +0800

    自动发布测试
    修改heroku的app名称
    添加临时测试1923字符

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,7 +8,7 @@ var pkg = require('./package.json');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
-const APP_TITLE = 'YAOPAI：一个全球预约摄影师平台';
+const APP_TITLE = 'YAOPAI：1923一个全球预约摄影师平台';
 
 var common = {
   entry: path.resolve(ROOT_PATH, 'app'),
