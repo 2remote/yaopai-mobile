8a5b1e4f425cc9767fd9bdb9f3e40dd0aabcfa93
Hu Songtao <idisblueflash@gmail.com>
2015-11-19 10:12:29 +0800

    添加对chai.expect的验证检验

--- tests/demo_test.js
+++ tests/demo_test.js
@@ -1,7 +1,13 @@
 import assert from 'assert';
+import { expect } from 'chai';
 
 describe('add', () => {
   it('adds', () => {
     assert.equal(1 + 1, 2);
+  });
+
+  it('Test chai expect', ()=> {
+    const comText = "YAOPAI";
+    expect(comText).to.be.a('string');
   });
 });
