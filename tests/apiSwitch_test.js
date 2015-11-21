2b7025e508b1a6b258eaa07b500b6d19038f696d
Haitao <liubiantao@gmail.com>
2015-11-21 17:13:49 +0800

    测试切换api

--- tests/apiSwitch_test.js
+++ tests/apiSwitch_test.js
@@ -15,8 +15,13 @@ describe('API switch', () => {
     const dev_host = 'http://yaopai-mobile-dev.heroku.com/#/work?_k=gn36vo';
     const pro_host = 'http://yaopai-mobile.heroku.com/#/work?_k=gn36vo';
 
-    const re = /yaopai-mobile\./i;
-    expect(dev_host.match(re)).to.equal(null);
-    expect(pro_host.match(re) != null).to.equal(true);
+    const re = /dev\./i;
+    expect(dev_host.match(re)).to.not.equal(null);
+    expect(pro_host.match(re)).to.equal(null);
+    expect('m.aiyaopai.com/'.match(re)).to.equal(null);
+    expect('dev.m.aiyaopai.com/'.match(re)).to.not.equal(null);
+    expect('dev.manage.aiyaopai.com'.match(re)).to.not.equal(null);
+    expect('manage.aiyaopai.com'.match(re)).to.equal(null);
+
   });
 });
