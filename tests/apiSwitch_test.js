f1af1ff219d9b716296d3415a8a0f84ee3d496a7
Haitao <liubiantao@gmail.com>
2015-11-23 10:35:21 +0800

    删掉冗余代码，增加PC端本地host「0.0.0.0:5000」判断

--- tests/apiSwitch_test.js
+++ tests/apiSwitch_test.js
@@ -15,6 +15,7 @@ describe('API switch', () => {
     'localhost:8080',
     '192.168.3.2:5000/#/login_page',
     'http://yaopai-mobile-dev.herokuapp.com/',
+    'http://0.0.0.0:5000',
     'dev.p.aiyaopai.com'
   ];
 
@@ -24,21 +25,6 @@ describe('API switch', () => {
     'http://yaopai-mobile.herokuapp.com/',
     'p.aiyaopai.com'
   ];
-
-  it('will get right local host', () => {
-    const dev_host = 'http://yaopai-mobile-dev.heroku.com/#/work?_k=gn36vo';
-    const pro_host = 'http://yaopai-mobile.heroku.com/#/work?_k=gn36vo';
-
-    const re = /dev\.|192\.|localhost/i;
-    expect(dev_host).to.match(re);
-    expect(pro_host).to.not.match(re);
-    expect('m.aiyaopai.com/').to.not.match(re);
-    expect('dev.m.aiyaopai.com/').to.match(re);
-    expect('dev.manage.aiyaopai.com').to.match(re);
-    expect('manage.aiyaopai.com').to.not.match(re);
-    expect('localhost:8080').to.match(re);
-    expect('192.168.3.2:5000/#/login_page').to.match(re);
-  });
 
   it('has hosts', () => {
     devHosts.forEach((host, i) => {
@@ -84,7 +70,7 @@ function isProdHost(host) {
 }
 
 function isDevHost(host) {
-  const re = /dev\.|192\.|localhost/i;
+  const re = /dev\.|192\.|localhost|0\./i;
   const founds = host.match(re);
   if (founds != null) {
     return true;
