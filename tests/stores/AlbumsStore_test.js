f36126f8895ac85bafe03e717e1c978ae26aed00
Hu Songtao <idisblueflash@gmail.com>
2015-11-23 15:50:27 +0800

    添加method: on add success测试

--- tests/stores/AlbumsStore_test.js
+++ tests/stores/AlbumsStore_test.js
@@ -1,15 +1,27 @@
 import Reflux from 'reflux';
-import { storeIsDefined, storeHasData, storeHasMethod } from '../refluxTestHelpers';
-import { expect } from 'chai';
+import {
+  storeIsDefined, storeHasData, storeHasMethod
+}
+from '../refluxTestHelpers';
+import {
+  expect
+}
+from 'chai';
+
 import AlbumsStore from '../../app/stores/AlbumsStore';
 
 describe('Albums Store Test', () => {
-  it('has store', ()=> {
+  beforeEach(() => {
+    AlbumsStore.data.hintMessage = '';
+    AlbumsStore.data.flag = '';
+  });
+
+  it('has store', () => {
     storeIsDefined(AlbumsStore);
     storeHasData(AlbumsStore);
   });
 
-  it('has methods', ()=> {
+  it('has methods', () => {
     const methods = [
       'onFailed',
       'onAddsuccess',
@@ -23,16 +35,35 @@ describe('Albums Store Test', () => {
       'offSaleSuccess',
       'onRecommendListSuccess'
     ];
-    methods.forEach((method) =>{
-      storeHasMethod(AlbumsStore, method);  
+    methods.forEach((method) => {
+      storeHasMethod(AlbumsStore, method);
     })
   });
 
-  it('works on failed', ()=> {
+  it('works on failed', () => {
     expect(AlbumsStore.data.hintMessage).is.empty;
     expect(AlbumsStore.data.flag).is.empty;
     AlbumsStore.onFailed();
     expect(AlbumsStore.data.hintMessage).to.equal('网络错误');
     expect(AlbumsStore.data.flag).to.equal('failed');
+  });
+
+  it('works on add success', () => {
+    let res = {
+      Success: true
+    };
+    AlbumsStore.onAddSuccess(res);
+    expect(AlbumsStore.data.hintMessage).is.empty;
+    expect(AlbumsStore.data.flag).to.equal('add');
+
+    const errorMsg = 'error message';
+    res = {
+      Success: false,
+      ErrorMsg: errorMsg
+    };
+
+    AlbumsStore.onAddSuccess(res);
+    expect(AlbumsStore.data.hintMessage).to.equal(errorMsg);
+    expect(AlbumsStore.data.flag).to.equal('add');
   });
 });
