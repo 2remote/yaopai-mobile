8887db7044a54ac071fed70332af8fffc2839bca
yangyunxin <850801584@qq.com>
2015-11-24 14:25:38 +0800

    手机号组件验证结束

--- tests/components/loginPage/LoginForm_test.js
+++ tests/components/loginPage/LoginForm_test.js
@@ -22,10 +22,20 @@ describe('登陆页 LoginForm组件', () => {
   });
 
   describe('手机输入框组件', () => {
+    // 获取手机号虚拟dom
+    let mobileNumber = component.refs.mobileNumber;
     it ('手机号dom存在', () => {
-      // 获取手机号虚拟dom
-      let mobileNumber = component.refs.mobileNumber;
       expect(mobileNumber.getDOMNode().placeholder).to.equal('手机号');
+    });
+
+    it ('输入号码后，改变state', () => {
+      let initNumber = component.state.userName;
+      // 验证手机号初始值是不是为空
+      expect(initNumber).to.be.empty;
+      // 模拟输入事件
+      Simulate.change(mobileNumber.getDOMNode(), {target: {value: '18883283605'}});
+      // 验证手机号是否改变
+      expect(component.state.userName).to.equal('18883283605');
     });
   });
 });
