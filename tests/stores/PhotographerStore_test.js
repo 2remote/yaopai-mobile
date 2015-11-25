a01fc7ea899af5b2e891701b992e8ff264a098af
Hu Songtao <idisblueflash@gmail.com>
2015-11-25 21:25:58 +0800

    初始化 PhotographerStore 测试

---
+++ tests/stores/PhotographerStore_test.js
@@ -0,0 +1,53 @@
+import Reflux from 'reflux';
+import {
+  storeIsDefined, storeHasData, storeHasMethod
+}
+from '../refluxTestHelpers';
+import {
+  expect
+}
+from 'chai';
+
+import PhotographerStore from '../../app/stores/PhotographerStore';
+
+describe('Photographer Store Test', () => {
+  const successfulRes = {
+    Success: true,
+    Result: [1, 2, 3]
+  };
+
+  const errorMsg = 'error message';
+
+  const failedRes = {
+    Success: false,
+    ErrorMsg: errorMsg
+  };
+
+  beforeEach(() => {
+    PhotographerStore.data.hintMessage = '';
+    PhotographerStore.data.flag = '';
+  });
+
+  it('has store', () => {
+    storeIsDefined(PhotographerStore);
+    storeHasData(PhotographerStore);
+  });
+
+  it('has methods', () => {
+    const methods = [
+      'onGetSuccess',
+      'onListSuccess',
+      'onFailed',
+      'onRecommendListSuccess'
+    ];
+    methods.forEach((method) => {
+      storeHasMethod(PhotographerStore, method);
+    })
+  });
+
+  it('works on get success', () => {
+    PhotographerStore.onGetSuccess(successfulRes);
+    expect(PhotographerStore.data.hintMessage).to.equal('');
+    expect(PhotographerStore.data.photographer).to.equal(successfulRes);
+  });
+});
