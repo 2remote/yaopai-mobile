1f2880cab64739241d32de50440572472eda1c43
Hu Songtao <idisblueflash@gmail.com>
2015-12-01 23:39:31 +0800

    测试初始化

---
+++ tests/stores/GetCodeStore_test.js
@@ -0,0 +1,72 @@
+import Reflux from 'reflux';
+import {
+  storeIsDefined,
+  storeHasData,
+  storeHasMethod,
+  storeCheckCommonUsage,
+  storeHasDefaultValue,
+  makeCheckStoreData,
+  makeStoreHasMethod
+}
+from '../refluxTestHelpers';
+import {
+  expect
+}
+from 'chai';
+
+import GetCodeStore from '../../app/stores/GetCodeStore';
+
+describe('Get Code Store Test', () => {
+  const successfulRes = {
+    Success: true,
+    Result: [1, 2, 3]
+  };
+
+  const errorMsg = 'error message';
+
+  const failedRes = {
+    Success: false,
+    ErrorMsg: errorMsg
+  };
+
+  const checkGetCodeStoreData = makeCheckStoreData(GetCodeStore);
+  const getCodeStoreHasMethod = makeStoreHasMethod(GetCodeStore);
+
+  const currentUserKey = 'yaopai_user';
+
+  beforeEach(() => {
+    GetCodeStore.data = {
+      hintMessage: '',
+      flag: '',
+      userId: '',
+      userName: '',
+      loginToken: '', //用户选择rememberme的时候返回
+      userType: '',
+      userState: '',
+      isLogin: false,
+      loginDate: '',
+      userKey: currentUserKey
+    };
+  });
+
+  it('has store', () => {
+    storeIsDefined(GetCodeStore);
+    storeHasData(GetCodeStore);
+  });
+
+
+
+  it('store has methods', () => {
+    const methods = [
+      'init',
+      'onBeginTelRegister',
+      'onTelRegisterSucess',
+      'onBeginTelRestPassword',
+      'onTelRestPasswordSuccess'
+    ];
+    methods.forEach((method) => {
+      getCodeStoreHasMethod(method);
+    })
+  });
+
+});
