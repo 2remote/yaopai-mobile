b0499d7c16dbbb6da6e04cbafa72ff36763bebf7
Hu Songtao <idisblueflash@gmail.com>
2015-12-04 23:40:24 +0800

    修复has methods不显示bug

--- tests/actions/AlbumsActions_test.js
+++ tests/actions/AlbumsActions_test.js
@@ -8,18 +8,15 @@ import {
 }
 from 'chai';
 
-import AlbumsStore from '../../app/stores/AlbumsStore';
 import AlbumsActions from '../../app/actions/AlbumsActions';
 
 const albumsActionsHasMethod = makeStoreHasMethod(AlbumsActions);
 describe('Albums Actions Test', () => {
 
   beforeEach(() => {
-    AlbumsStore.data.hintMessage = '';
-    AlbumsStore.data.flag = '';
   });
 
-  describe('has methods', () => {
+  it('has methods', () => {
     const methods = [
       'get',
       'add',
