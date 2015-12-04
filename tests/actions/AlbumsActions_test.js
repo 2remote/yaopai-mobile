44c9ad07c5087a80f974863cfed11b41990a1843
Hu Songtao <idisblueflash@gmail.com>
2015-12-05 01:00:05 +0800

    添加测试 getCategories
    没有发现测试action更好的办法，目前只能测试是否激活，意义不大。
    希望能测试传入的api和返回结果，目前还没有答案。

--- tests/actions/AlbumsActions_test.js
+++ tests/actions/AlbumsActions_test.js
@@ -13,8 +13,7 @@ import AlbumsActions from '../../app/actions/AlbumsActions';
 const albumsActionsHasMethod = makeStoreHasMethod(AlbumsActions);
 describe('Albums Actions Test', () => {
 
-  beforeEach(() => {
-  });
+  beforeEach(() => {});
 
   it('has methods', () => {
     const methods = [
@@ -32,5 +31,42 @@ describe('Albums Actions Test', () => {
     methods.forEach((method) => {
       albumsActionsHasMethod(method);
     })
+  });
+
+  describe('getCategories', () => {
+    let store = Reflux.createStore({
+      init: function() {
+        console.log('test store initialized');
+        this.listenTo(AlbumsActions.getCategories.success, this.onSuccess);
+        this.listenTo(AlbumsActions.getCategories.failed, this.onFailed);
+      },
+      onSuccess: function(data) {
+        console.log('store data success', data);
+        this.data = 'success';
+        this.trigger(this.data);
+      },
+      onFailed: function(data) {
+        console.log('store data failed', data);
+        this.data = 'failed';
+        this.trigger(this.data);
+      }
+    });
+
+    function ConsoleComponent() {
+      store.listen(function(status) {
+        console.log('status: ', status);
+      });
+    };
+
+    var consoleComponent = new ConsoleComponent();
+
+    it('works on success', (done) => {
+      try {
+        AlbumsActions.getCategories.success.trigger(4);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
   });
 });
