c45d3e84d13a96fd8088493862f568acbb2102e7
Hu Songtao <idisblueflash@gmail.com>
2015-12-07 23:42:38 +0800

    精简测试

--- tests/actions/AlbumsActions_test.js
+++ tests/actions/AlbumsActions_test.js
@@ -15,8 +15,10 @@ import AlbumsActions from '../../app/actions/AlbumsActions';
 
 const albumsActionsHasMethod = makeStoreHasMethod(AlbumsActions);
 describe('Albums Actions Test', () => {
-
-  beforeEach(() => {});
+  let data;
+  beforeEach(() => {
+    data ={}
+  });
 
   it('has methods', () => {
     const methods = [
@@ -36,39 +38,31 @@ describe('Albums Actions Test', () => {
     })
   });
 
-  describe('API.getCategories', () => {
-    const data = {
-      Fields : 'Id,Name,Sorting,Display,Views'
-    };
-    apiOk(API.ALBUMS.categories, data, '获取分类列表');
-  });
+  data = {
+    Fields : 'Id,Name,Sorting,Display,Views'
+  };
+  apiOk(API.ALBUMS.categories, data, 'API.getCategories 获取分类列表');
+  
+  data = {
+    Id: 2,
+    Fields: "Id,Title,UserId,CategoryId,Description,Service,Price,Cover,Photos.Id,Photos.AlbumsId,Photos.Url,Photos.Description,User.Id,User.NickName,User.Avatar"
+  };
+  apiOk(API.ALBUMS.get, data, 'API.get 使用ID＝2的作品测试get功能');
 
-  describe('API.get', () => {
-    const data = {
-      Id: 2,
-      Fields: "Id,Title,UserId,CategoryId,Description,Service,Price,Cover,Photos.Id,Photos.AlbumsId,Photos.Url,Photos.Description,User.Id,User.NickName,User.Avatar"
-    };
-    apiOk(API.ALBUMS.get, data, '使用ID＝2的作品测试get功能');
-  });
+  data = {
+    PageIndex:1,
+    PageSize:10,
+    CategoryId : null,
+    Fields : 'Id,Title,UserId,CategoryId,Description,Service,Price,Cover,Photos.Id,Photos.AlbumsId,Photos.Url,Photos.Description,User.Id,User.NickName,User.Avatar'
+  };
+  apiOk(API.ALBUMS.search, data, 'API.search search返回成功');
 
-  describe('API.search', () => {
-    const data = {
-      PageIndex:1,
-      PageSize:10,
-      CategoryId : null,
-      Fields : 'Id,Title,UserId,CategoryId,Description,Service,Price,Cover,Photos.Id,Photos.AlbumsId,Photos.Url,Photos.Description,User.Id,User.NickName,User.Avatar'
-    };
-    apiOk(API.ALBUMS.search, data, 'search返回成功');
-  });
-
-  describe('API.recommendList', () => {
-    const data ={
-      HomeRecommended : true,
-      HomeSortingDesc : true,
-      PageIndex : 1,
-      PageSize : 8,
-      Fields : 'Id,Title,UserId,CategoryId,Description,Service,Price,Cover,Photos.Id,Photos.AlbumsId,Photos.Url,Photos.Description,User.Id,User.NickName,User.Avatar',
-    };
-    apiOk(API.ALBUMS.search, data, 'recommend list返回成功');
-  });
+  data ={
+    HomeRecommended : true,
+    HomeSortingDesc : true,
+    PageIndex : 1,
+    PageSize : 8,
+    Fields : 'Id,Title,UserId,CategoryId,Description,Service,Price,Cover,Photos.Id,Photos.AlbumsId,Photos.Url,Photos.Description,User.Id,User.NickName,User.Avatar',
+  };
+  apiOk(API.ALBUMS.search, data, 'API.recommendList recommend list返回成功');
 });
