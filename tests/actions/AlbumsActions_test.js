a1f7800a918ca618b0270f4a36133d201e9a8580
Hu Songtao <idisblueflash@gmail.com>
2015-12-07 21:13:04 +0800

    修复错误描述

--- tests/actions/AlbumsActions_test.js
+++ tests/actions/AlbumsActions_test.js
@@ -78,11 +78,11 @@ describe('Albums Actions Test', () => {
   });
 
   it('update', () => {
-    expect(true, '跳过update，移动端没有添加作品功能。').to.equal(true);
+    expect(true, '跳过update，移动端没有更新作品功能。').to.equal(true);
   });
 
   it('delete', () => {
-    expect(true, '跳过delete，移动端没有添加作品功能。').to.equal(true);
+    expect(true, '跳过delete，移动端没有删除作品功能。').to.equal(true);
   });
 
   it('getMyAlbums', () => {
