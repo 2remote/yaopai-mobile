bbb9007805489cf055d9d29716079d6770bee4b4
Hu Songtao <idisblueflash@gmail.com>
2015-12-09 21:21:23 +0800

    移除测试数据

--- app/components/UserCenterPage/JobTicketList.js
+++ app/components/UserCenterPage/JobTicketList.js
@@ -5,25 +5,7 @@ import JobTicketRow  from './JobTicketRow' ;
 var JobTicketList = React.createClass({
   getDefaultProps: function() {
     return {
-      data: [
-        {
-          grapherAvatar: 'imgs/default/chenmingqiao-small.jpg',
-          grapherName: '陈明乔',
-          userName: '马晓驰',
-          userAvatar: 'imgs/default/maxiaochi-small.jpg',
-          userPhone: '13113658516',
-          userBookDate: '2015/10/19',
-          suggestPrice: 3000
-        },{
-          grapherAvatar: 'imgs/default/yinchao-small.jpg',
-          grapherName: '尹超',
-          userName: '张扬',
-          userAvatar: 'imgs/default/maxiaochi-small.jpg',
-          userPhone: '13113658516',
-          userBookDate: '2015/9/19',
-          suggestPrice: '面议'
-        }
-      ]
+      data: []
     };
   },
 
