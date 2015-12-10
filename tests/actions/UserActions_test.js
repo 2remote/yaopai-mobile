b0f13e1e2edfac4acc3170aec404b051e506ed25
Hu Songtao <idisblueflash@gmail.com>
2015-12-10 17:13:09 +0800

    添加测试api.user.login

--- tests/actions/UserActions_test.js
+++ tests/actions/UserActions_test.js
@@ -14,7 +14,7 @@ import API from '../../app/api';
 import UserActions from '../../app/actions/UserActions';
 
 const userActionsHasMethod = makeStoreHasMethod(UserActions);
-describe('Albums Actions Test', () => {
+describe('User Actions Test', () => {
   let data;
   beforeEach(() => {
     data ={}
@@ -37,4 +37,13 @@ describe('Albums Actions Test', () => {
       userActionsHasMethod(method);
     })
   });
+
+  const apiFlag = true;
+  if ( apiFlag == true ){
+    data = {
+      loginname: '13552987637',
+      password: 'woshifox',
+    };
+    apiOk(API.USER.login, data, 'API.login 用户登录');
+  }
 });
