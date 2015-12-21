ece907fcb908810beeceb770edc1870767044ba8
Hu Songtao <idisblueflash@gmail.com>
2015-12-21 23:33:40 +0800

    清理代码

--- app/components/Index/ImageBoxLine.js
+++ app/components/Index/ImageBoxLine.js
@@ -8,10 +8,6 @@ var ImageBoxGrid = React.createClass({
     var deviceWidth = parseInt(window.innerWidth);
     return {
       deviceWidth: deviceWidth,
-      workCover:  [{
-        src: "imgs/indexPage/view-picture-image.jpg", 
-        srcset: "imgs/indexPage/view-picture-image@2X.jpg 2x", 
-        url: "/interview"}],
       works: []
     };
   },
@@ -21,22 +17,6 @@ var ImageBoxGrid = React.createClass({
       width: deviceWidth/3,
       height: deviceWidth/3
     };
-    var coverStyle = {
-      width: deviceWidth/3,
-      height: deviceWidth/3-0.9
-    };
-    
-    var workCover = this.props.workCover.map(function(img){
-      return (
-        <li style={coverStyle} className="imageCell">
-          <Link style={coverStyle} to={img.url} >
-            <img style={{width: deviceWidth/3,height: deviceWidth/3}} 
-              src={img.src} 
-              srcSet={img.srcSet} />
-          </Link>
-        </li>
-      );
-    });
 
     var imgNodes = this.props.works.map(function(work, i){
       return (
@@ -47,10 +27,6 @@ var ImageBoxGrid = React.createClass({
         </li>
       );  
     });
-
-    var firstPic = imgNodes.shift();
-    imgNodes.unshift(workCover);
-    imgNodes.unshift(firstPic);
 
     return (
       <ul className="imageBoxGrid">
