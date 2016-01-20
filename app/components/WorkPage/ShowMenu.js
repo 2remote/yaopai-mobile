9489dffffaf7c260e063da9fda61de4547b6ea9e
Haitao <liubiantao@gmail.com>
2016-01-20 15:16:14 +0800

    优化翻转动画在微信里面的兼容性

--- app/components/WorkPage/ShowMenu.js
+++ app/components/WorkPage/ShowMenu.js
@@ -14,14 +14,16 @@ var ShowMenu = React.createClass({
     this.setState({showState: !this.state.showState});
   },
   render: function () {
-    var transform = this.state.showState ? 'rotateX(180deg)' : 'rotateX(0deg)';
+    var transform = this.state.showState ? 'scaleY(-1)' : '';
     var style = {
       position: 'fixed',
       top: '22px',
       right: '22px',
       zIndex: '99',
       transform: transform,
+      WebkitTransform: transform,
       transition: '1s',
+      WebkitTransition: '1s',
     };
 
     return (
