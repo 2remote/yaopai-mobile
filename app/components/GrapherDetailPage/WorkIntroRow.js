ccacb71f7e97a1fcbb0c089a0dda7a2718777fb4
Haitao <liubiantao@gmail.com>
2016-01-20 14:32:38 +0800

    调整样式

--- app/components/GrapherDetailPage/WorkIntroRow.js
+++ app/components/GrapherDetailPage/WorkIntroRow.js
@@ -12,16 +12,52 @@ var WorkIntroRow = React.createClass({
     };
   },
   render: function() {
+    const style = {
+      price: {
+        position: 'absolute',
+        right: 20,
+        marginTop: 15,
+        color: 'white'
+      },
+      photoAmount: {
+        position: 'absolute',
+        right: 20,
+        bottom: 20,
+        color: 'white'
+      },
+      topMask: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        height: 40,
+        background: '-webkit-gradient(linear, left bottom, left top, from(rgba(0,0,0,0)), to(rgba(0,0,0,0.8)))',
+        background: '-webkit-linear-gradient(bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.8) 100%)',
+        background: 'linear-gradient(to top, rgba(0,0,0,0) 0%,rgba(0,0,0,0.8) 100%)',
+      },
+      bottomMask:{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        height: 40,
+        marginTop: -47,
+        background: '-webkit-gradient(linear, left top, left bottom, from(rgba(0,0,0,0)), to(rgba(0,0,0,0.8)))',
+        background: '-webkit-linear-gradient(top, rgba(0,0,0,0) 0%, rgba(0,0,0,0.8) 100%)',
+        background: 'linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.8) 100%)',
+      }
+    };
     const cover = imgModifier(this.props.data.Cover,"workCover");
     return (
       <div className="workIntroRow" style={{width:'100%',height:210/375*innerWidth+80,textAlign:'center'}}>
         <Link to={"/workDetail/" + this.props.data.Id}>
-          <span>{"¥ "+this.props.data.Price}</span>
+          <div style={style.topMask} />
+          <span style={style.price}>{"¥ "+this.props.data.Price}</span>
           <img 
             style={{width:'100%',height:210/375*innerWidth}}
             ref="workImage"
             src={cover} />
-          <span>{this.props.data.Photos.length + "P"}</span>
+          <div style={style.bottomMask}>
+            <span style={style.photoAmount} >{this.props.data.Photos.length + " P"}</span>
+          </div>
         </Link>
         <span 
           style={{marginBottom:1, color:'green'}}
