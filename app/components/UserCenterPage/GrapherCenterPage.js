793f849b71d7415049d7b8c229f199118ef418e7
Haitao <liubiantao@gmail.com>
2016-01-23 16:52:31 +0800

    更新图标

--- app/components/UserCenterPage/GrapherCenterPage.js
+++ app/components/UserCenterPage/GrapherCenterPage.js
@@ -105,9 +105,9 @@ var GrapherCenterPage = React.createClass({
         className="grapherCenterPage">
         <HamburgMenu />
         <UserAvatarBox background={true} data={this.state.userInfo}/>
-        {makeUiButton('edit_icon', '订单管理', 'grapher_tickets', 'react-router')}
-        {makeUiButton('edit_icon', '我的主页', 'grapherDetail/'+ this.state.userInfo.userId, 'react-router')}
-        {makeUiButton('phone_circle_icon', '联系客服', 'tel:+86-0371-6533-7727')}  
+        {makeUiButton('order_icon', '订单管理', 'grapher_tickets', 'react-router')}
+        {makeUiButton('home_icon', '我的主页', 'grapherDetail/'+ this.state.userInfo.userId, 'react-router')}
+        {makeUiButton('customer_icon', '联系客服', 'tel:+86-0371-6533-7727')}  
       </div>
     );
   }
