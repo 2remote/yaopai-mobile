c246d16ecadf660f3a05f0adb24df9f7637324bc
Hu Songtao <idisblueflash@gmail.com>
2016-02-03 20:04:23 +0800

    添加修改性别页面

--- app/index.js
+++ app/index.js
@@ -28,6 +28,7 @@ import UserCenterPage from './components/UserCenterPage';
 import UserTicketsPage from './components/UserCenterPage/UserTicketsPage';
 import UserEditProfile from './components/UserCenterPage/UserEditProfilePage';
 import UserNickNameChange from './components/UserCenterPage/UserNickNameChangePage';
+import UserGenderChange from './components/UserCenterPage/UserGenderChangePage';
 
 import GrapherCenterPage from './components/UserCenterPage/GrapherCenterPage';
 import GrapherTicketsPage from './components/UserCenterPage/GrapherTicketsPage';
@@ -66,6 +67,7 @@ function main(){
       <Route path="/user_tickets" component={UserTicketsPage} />
       <Route path="/user_edit_profile" component={UserEditProfile} />
       <Route path="/user_nickname_change" component={UserNickNameChange} />
+      <Route path="/user_gender_change" component={UserGenderChange} />
 
       <Route path="/grapher_center" component={GrapherCenterPage} />
       <Route path="/grapher_tickets" component={GrapherTicketsPage} />
