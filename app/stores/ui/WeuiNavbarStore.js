b689e8be29dc403313bb41f14c394bc6637671da
Peter Wong <amile1860@163.com>
2016-03-04 11:08:56 +0800

    增加weui-navbar的store

---
+++ app/stores/ui/WeuiNavbarStore.js
@@ -0,0 +1,23 @@
+import Reflux from 'reflux';
+import WeuiNavbarActions from '../../actions/ui/WeuiNavbarActions';
+
+let WeuiNavbarStore = Reflux.createStore({
+  items: {
+
+  },
+  listenables: [WeuiNavbarActions],
+  init(props) {
+    console.log('[WeuiNavbarActions]', this.props);
+    console.log('[WeuiNavbarActions]', props);
+  },
+  onSetOn(tabId) {
+    // 清空所有状态(转为false)
+    this.items.foreach((item)=>{
+      item = false;
+    });
+    // 设置选中tab为true
+    this.items[tabId] = !this.items[tabId];
+    this.trigger(this.items);
+  }
+});
+export { WeuiNavbarStore as default };
