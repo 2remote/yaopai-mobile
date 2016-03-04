da0263b7d42895adeefc261d9ddf048ce1f0ef23
Peter Wong <amile1860@163.com>
2016-03-04 09:56:19 +0800

    增加订单列表Component(10%)

---
+++ app/components/Center/Grapher/OrderList.js
@@ -0,0 +1,19 @@
+'use strict';
+import React from 'react';
+
+/**
+ * 订单列表:
+ *   订单为空时显示:
+ *   订单类型:
+ */
+class OrderList extends React.Component {
+  render() {
+    return (
+      <div className="orderlist">
+        This is an order list.
+      </div>
+    );
+  }
+}
+
+export { OrderList as default}
