a5a77c89fd39976d1bd51dddd0be1dbe2a02afad
Peter Wong <amile1860@163.com>
2016-03-07 14:45:37 +0800

    增加订单列表布局包含YPUIOrderCard

--- app/components/layout/center/user/order/OrderListLayout.jsx
+++ app/components/layout/center/user/order/OrderListLayout.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import YPUIOrderCard from  '../../../../UI/YPUIOrderCard.jsx';
+
+let sampleOrderList = [{
+  name: 'YaoPai',
+  tel: '13311223344'
+}, {
+  name: 'YaoPai2',
+  tel: '13311223344'
+}];
+
 const OrderListLayout = () => (
-  <p>OrderListLayout</p>
+  <div>
+    {sampleOrderList.map((order, index) =>
+      <YPUIOrderCard order={order} key={index}/>
+    )}
+  </div>
 );
 
 export default OrderListLayout;
