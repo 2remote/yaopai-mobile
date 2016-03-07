e9b856768dc93280e99941f9d4368cbd155abac5
Peter Wong <amile1860@163.com>
2016-03-07 15:12:47 +0800

    增加临时静态界面的sample数据填充与展示

--- app/components/UI/YPUIOrderCard.jsx
+++ app/components/UI/YPUIOrderCard.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 
+/**
+ * 本component是个人中心中订单的UI组件,用于展示不同状态的订单UI
+ * @param order: 订单信息,包含以下内容:
+ * order.id: 订单ID
+ * order.status: 订单状态,分为:
+ * order.userId: 订单-用户ID
+ * order.grapher.id: 订单-摄影师ID
+ * order.grapher.avatar: 订单-摄影师头像
+ * order.grapher.name: 订单-摄影师昵称
+ * order.grapher.phone: 订单-摄影师电话号码
+ * order.title: 订单标题
+ * order.time: 预约时间
+ * order.price: 订单价格
+ * order.payedAmount: 订单已付价格
+ * @constructor
+ */
 const YPUIOrderCard = ({order}) => (
-  <p style={{
+  <div style={{
     border: '3px solid red',
     marginBottom: '15px'
   }}>
-    <div>name: {order.name}</div>
-    <div>tel: {order.tel}</div>
-  </p>
+    <div className="grapherAvatar">
+      <img src={order.grapher.avatar} style={{
+        width: '100%'
+      }}/>
+      <br/>
+      <div>{order.grapher.name}</div>
+    </div>
+    <div className="orderDetail">
+      <div>{order.title}</div>
+      <div>预约时间:{order.time}</div>
+      <div>价格:{order.price}</div>
+      <div>实付:{order.payedAmount}</div>
+    </div>
+    <div className="orderOperate">
+      <div><a href={`tel:${order.grapher.phone}`}>联系</a></div>
+      <div><button type="button">取消</button></div>
+    </div>
+  </div>
 );
 
 export default YPUIOrderCard;
--- app/components/layout/center/user/order/OrderListLayout.jsx
+++ app/components/layout/center/user/order/OrderListLayout.jsx
@@ -2,12 +2,50 @@ import React from 'react';
 
 import YPUIOrderCard from  '../../../../UI/YPUIOrderCard.jsx';
 
+//* order.id: 订单ID
+//* order.status: 订单状态,分为:
+//* order.userId: 订单-用户ID
+//* order.grapher.id: 订单-摄影师ID
+//* order.grapher.avatar: 订单-摄影师头像
+//* order.grapher.name: 订单-摄影师昵称
+//* order.grapher.phone: 订单-摄影师电话号码
+//* order.title: 订单标题
+//* order.time: 预约时间
+//* order.price: 订单价格
+//* order.payedAmount: 订单已付价格
+
 let sampleOrderList = [{
-  name: 'YaoPai',
-  tel: '13311223344'
+  id: 1,
+  status: 1,
+  user: {
+    id: 1
+  },
+  grapher: {
+    id: 1001,
+    avatar: 'http://7xrgj5.com1.z0.glb.clouddn.com/35/009098bc-6d59-4443-93ec-3e9f4d5bb277.jpg?imageMogr2/auto-orient/gravity/Center/thumbnail/!78x78r/crop/78x78/interface/1',
+    name: '糖宝',
+    phone: '17600801640'
+  },
+  title: '少女写真(闺蜜组)',
+  time: '2016-03-07',
+  price: '1267',
+  payedAmount: '1267'
 }, {
-  name: 'YaoPai2',
-  tel: '13311223344'
+  id: 2,
+  status: 1,
+  user: {
+    id: 2
+  },
+  grapher: {
+    id: 1002,
+    avatar: 'http://7xrgj5.com1.z0.glb.clouddn.com/35/009098bc-6d59-4443-93ec-3e9f4d5bb277.jpg?imageMogr2/auto-orient/gravity/Center/thumbnail/!78x78r/crop/78x78/interface/1',
+    name: '糖宝2',
+    phone: '17600801640'
+  },
+  title: '少女写真(闺蜜组)',
+  time: '2016-03-09',
+  price: '1267',
+  payedAmount: '1267'
 }];
 
 const OrderListLayout = () => (
