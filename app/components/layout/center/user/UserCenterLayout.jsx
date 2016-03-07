59aae18dcd3dcc13779130ec738b2679190a6dbe
Peter Wong <amile1860@163.com>
2016-03-07 11:53:27 +0800

    增加订单页的数据和跳转

--- app/components/layout/center/user/UserCenterLayout.jsx
+++ app/components/layout/center/user/UserCenterLayout.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import WeuiCells from '../../../UI/WeuiCells';
+
+let list1 = [{
+  icon: 'wallet_icon',
+  title: '我的收藏',
+  href: '/center/u/collect'
+}];
+
+let list2 = [{
+  icon: 'wallet_icon',
+  title: '个人资料',
+  href: '/center/u/profile'
+},{
+  icon: 'wallet_icon',
+  title: '我的订单',
+  href: '/center/u/order'
+},{
+  icon: 'wallet_icon',
+  title: '钱包',
+  money: '0.00',
+  href: '/center/u/purse'
+},{
+  icon: 'wallet_icon',
+  title: '联系客服'
+}];
+
 const UserCenterLayout = () => (
-  <p>UserCenterLayout</p>
+  <div>
+    <WeuiCells cellList={list1}/>
+    <WeuiCells cellList={list2}/>
+  </div>
 );
 
 export default UserCenterLayout;
