047172c7a1112ee65f6779a95d48cd0dd54e11f3
可乐 <zaxlct@foxmail.com>
2016-03-07 18:01:47 +0800

    订单页面静态页面搭建
    只写了html部分，css暂时没写

--- app/components/layout/center/user/order/OrderSubmitLayout.jsx
+++ app/components/layout/center/user/order/OrderSubmitLayout.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const OrderSubmitLayout = () => (
-  <p>OrderSubmitLayout</p>
+  <div className="container">
+		<header>
+			<h3>当前订单</h3>
+			<img src="" alt=""/>
+			<span>少女写真(闺蜜组)</span>
+		</header>
+
+		<article>
+			<p>
+				<i className='icon grapher_icon' />
+				<input type="text"/>
+			</p>
+			<p>
+				<i className='icon grapher_icon' />
+				<input type="text"/>
+			</p>
+			<p>
+				<i className='icon grapher_icon' />
+				<input type="text"/>
+			</p>
+			<p>
+				<i className='icon grapher_icon' />
+				<input type="text"/>
+			</p>
+
+			<button>提交订单</button>
+		</article>
+	</div>
 );
 
 export default OrderSubmitLayout;
