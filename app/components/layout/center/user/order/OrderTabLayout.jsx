082bd076595e87ff4ae6f80931146cf3b1cbfd4e
Peter Wong <amile1860@163.com>
2016-03-07 14:44:02 +0800

    移除订单界面跳转，修改OrderTab内增加OrderListLayout

--- app/components/layout/center/user/order/OrderTabLayout.jsx
+++ app/components/layout/center/user/order/OrderTabLayout.jsx
@@ -1,35 +1,31 @@
 import React from 'react';
 
+import OrderListLayout from './OrderListLayout.jsx';
 import WeuiNavbar from '../../../../UI/WeuiNavbar';
 
 let navList = [{
   id: '1',
-  text: '待付款',
-  href: '#/center/u/order/unpayed'
+  text: '待付款'
 },{
   id: '2',
-  text: '待确认',
-  href: '#/center/u/order/unconfirmed'
+  text: '待确认'
 },{
   id: '3',
-  text: '进行中',
-  href: '#/center/u/order/ongoing'
+  text: '进行中'
 },{
   id: '4',
-  text: '已完成',
-  href: '#/center/u/order/completed'
+  text: '已完成'
 },{
   id: '5',
-  text: '已关闭',
-  href: '#/center/u/order/closed'
+  text: '已关闭'
 }];
 
-const OrderTabLayout = ({children}) => (
+const OrderTabLayout = () => (
   <div className="weui_tab">
     <WeuiNavbar list={navList}>
     </WeuiNavbar>
     <div className="weui_tab_bd">
-      {children}
+      <OrderListLayout />
     </div>
   </div>
 );
