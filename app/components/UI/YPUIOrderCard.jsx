b05904a76a99fcea69d3264b9679a43c8ca6ed36
Peter Wong <amile1860@hotmail.com>
2016-03-10 18:21:53 +0800

    调整订单卡片为使用weui_panel

--- app/components/UI/YPUIOrderCard.jsx
+++ app/components/UI/YPUIOrderCard.jsx
@@ -18,7 +18,33 @@ import React from 'react';
  */
 
 const YPUIOrderCard = ({order}) => (
-	<div></div>
+	<div className="weui_panel weui_panel_access">
+    <div className="weui_panel_bd">
+      <a className="weui_media_box weui_media_appmsg">
+        <div className="weui_media_hd">
+          <img className="weui_media_appmsg_thumb" src={order.grapher.avatar} alt/>
+        </div>
+        <div className="weui_media_bd">
+          <h4 className="weui_media_title">
+            {order.title}
+          </h4>
+          <p className="weui_media_desc">预约时间:{order.time}</p>
+          <p className="weui_media_desc">价格:{order.price}&nbsp;&nbsp;&nbsp;实付:{order.payedAmount}</p>
+        </div>
+      </a>
+    </div>
+    <div className="weui_panel_bd">
+      <div className="yp_media_box">
+        <div><a href={`tel:${order.grapher.phone}`}><i className="icon phone_icon" />联系{order.grapher.name}</a></div>
+        <div className="flex_spring"></div>
+        <div>
+          <button className="weui_btn weui_btn_mini weui_btn_default">退款</button>
+          <span>&nbsp;&nbsp;</span>
+          <button className="weui_btn weui_btn_mini weui_btn_primary">&nbsp;&nbsp;收片&nbsp;&nbsp;</button>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export {YPUIOrderCard as default};
