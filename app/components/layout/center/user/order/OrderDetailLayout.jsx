520b0b6d2764fbdbe5b857203e8be1bef204c840
可乐 <zaxlct@foxmail.com>
2016-03-11 16:36:08 +0800

    完成 支付流程布局

--- app/components/layout/center/user/order/OrderDetailLayout.jsx
+++ app/components/layout/center/user/order/OrderDetailLayout.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 const OrderDetailLayout = () => (
   <div className="OrderDetailLayout">
 		<div className="weui_cells_title">支付流程说明</div>
+		<section className="icon_box color_gray">
+			<span><i className="icon phone" /><br/>提单
+			</span><span><i className="icon phone" /><br/>付款
+			</span><span><i className="icon phone" /><br/>拍摄
+			</span><span><i className="icon phone" /><br/>收片</span>
+		</section>
 		<div className="weui_cells_title">订单详情</div>
 
-		<section className="icon_box">
-			<i className="icon phone" /><i className="icon phone" /><i className="icon phone" /><i className="icon phone" />
-		</section>
+
 	</div>
 );
 
