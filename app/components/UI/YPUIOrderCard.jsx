8a768c915c98cdb3552ac5a7cf19a76d37cae414
可乐 <zaxlct@foxmail.com>
2016-03-14 14:06:18 +0800

    点击“收片”后弹出框

--- app/components/UI/YPUIOrderCard.jsx
+++ app/components/UI/YPUIOrderCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Dialog, Button} from 'react-weui';
+const {Alert, Confirm} = Dialog;
 
 /**
  * 本component是个人中心中订单的UI组件,用于展示不同状态的订单UI
@@ -17,108 +19,151 @@ import React from 'react';
  * @constructor
  */
 
-const YPUIOrderCard = ({order}) => (
-	<div className="weui_panel weui_panel_access">
-    <div className="weui_panel_bd">
-      <a className="weui_media_box weui_media_appmsg">
-        <div className="weui_media_hd">
-          <img className="weui_media_appmsg_thumb" src={order.grapher.avatar} alt/>
-        </div>
-        <div className="weui_media_bd">
-          <h4 className="weui_media_title">
-            {order.title}
-          </h4>
-          <p className="weui_media_desc">
-            <i className="icon font_small book_icon ">&nbsp;&nbsp;</i>
-            预约时间：{order.time}
-          </p>
-          <p className="weui_media_desc">
-            <i className="icon font_small price_icon">&nbsp;&nbsp;</i>
-            价格：￥{order.price}
-            &nbsp;&nbsp;&nbsp;
-            实付：<span className="color_red">￥{order.payedAmount}</span>
-          </p>
-        </div>
-        <i className="icon youjiantou top_right_icon color_gray"></i>
-        {/*<i className="icon close_icon top_right_icon color_gray"></i>*/}
-      </a>
-    </div>
-    {/*某一天需要分离出这个分割线的话，就做成单独的UI Component*/}
-    <hr className="separator" />
-    {/* 代付款 */}
-    <div className="weui_panel_bd">
-      <div className="yp_media_box">
-        <div>
-          <a href={`tel:${order.grapher.phone}`} className="color_gray">
-            <i className="icon phone_icon" />
-            联系{order.grapher.name}
+class YPUIOrderCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showConfirm: false,
+      confirm: {
+        title: '温馨提示',
+        buttons: [
+          {
+            type: 'default',
+            label: '取消',
+            onClick: this.hideConfirm
+          },
+          {
+            type: 'primary',
+            label: '确认',
+            onClick: this.hideConfirm
+          }
+        ]
+      }
+    };
+
+  }
+
+  showConfirm = (e) => {
+    this.setState({showConfirm: true});
+  };
+
+  hideConfirm = (e) => {
+    this.setState({showConfirm: false});
+  };
+
+  render() {
+    const {order} = this.props;
+    return (
+      <div className="weui_panel weui_panel_access">
+        <div className="weui_panel_bd">
+          <a className="weui_media_box weui_media_appmsg">
+            <div className="weui_media_hd">
+              <img className="weui_media_appmsg_thumb" src={order.grapher.avatar} alt/>
+            </div>
+            <div className="weui_media_bd">
+              <h4 className="weui_media_title">
+                {order.title}
+              </h4>
+              <p className="weui_media_desc">
+                <i className="icon font_small book_icon ">&nbsp;&nbsp;</i>
+                预约时间：{order.time}
+              </p>
+              <p className="weui_media_desc">
+                <i className="icon font_small price_icon">&nbsp;&nbsp;</i>
+                价格：￥{order.price}
+                &nbsp;&nbsp;&nbsp;
+                实付：<span className="color_red">￥{order.payedAmount}</span>
+              </p>
+            </div>
+            <i className="icon youjiantou top_right_icon color_gray"></i>
+            {/*<i className="icon close_icon top_right_icon color_gray"></i>*/}
           </a>
         </div>
-        <div className="flex_spring"></div>
-        <div>
-          <button className="weui_btn weui_btn_mini weui_btn_primary">
-            去支付
-          </button>
+        {/*某一天需要分离出这个分割线的话，就做成单独的UI Component*/}
+        <hr className="separator" />
+        {/* 代付款 */}
+        <div className="weui_panel_bd">
+          <div className="yp_media_box">
+            <div>
+              <a href={`tel:${order.grapher.phone}`} className="color_gray">
+                <i className="icon phone_icon" />
+                联系{order.grapher.name}
+              </a>
+            </div>
+            <div className="flex_spring"></div>
+            <div>
+              <button className="weui_btn weui_btn_mini weui_btn_primary">
+                去支付
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* 待确认 */}
+        <div className="weui_panel_bd">
+          <div className="yp_media_box">
+            <div>
+              <a href={`tel:${order.grapher.phone}`} className="color_gray">
+                <i className="icon phone_icon" />
+                联系{order.grapher.name}
+              </a>
+            </div>
+            <div className="flex_spring"></div>
+            <div>
+              <button className="weui_btn weui_btn_mini weui_btn_default">
+                退款
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* 进行中 */}
+        <div className="weui_panel_bd">
+          <div className="yp_media_box">
+            <div>
+              <a href={`tel:${order.grapher.phone}`} className="color_gray">
+                <i className="icon phone_icon" />
+                联系{order.grapher.name}
+              </a>
+            </div>
+            <div className="flex_spring"></div>
+            <div>
+              <button className="weui_btn weui_btn_mini weui_btn_default">
+                退款
+              </button>
+              <span>&nbsp;&nbsp;</span>
+              {/*<button className="weui_btn weui_btn_mini weui_btn_primary">
+                &nbsp;&nbsp;收片&nbsp;&nbsp;
+              </button>*/}
+              <Button type="primary" className="weui_btn weui_btn_mini" onClick={this.showConfirm}>收片</Button>
+              <Confirm
+                show={this.state.showConfirm}
+                title={this.state.confirm.title}
+                buttons={this.state.confirm.buttons}>
+                请您收到照片后再点击“确定”，点击“确定”后将把款打到摄影师的账户中！
+              </Confirm>
+            </div>
+          </div>
+        </div>
+        {/* TODO: 已完成 */}
+        {/* 已关闭 */}
+        <div className="weui_panel_bd">
+          <div className="yp_media_box">
+            <div>
+              <a href={`tel:${order.grapher.phone}`} className="color_gray">
+                <i className="icon phone_icon" />
+                联系{order.grapher.name}
+              </a>
+            </div>
+            <div className="flex_spring"></div>
+            <div>
+              <span className="color_gray">已取消订单</span>
+              <span className="color_red">退款成功</span>
+              <span className="color_green">已完成</span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    {/* 待确认 */}
-    <div className="weui_panel_bd">
-      <div className="yp_media_box">
-        <div>
-          <a href={`tel:${order.grapher.phone}`} className="color_gray">
-            <i className="icon phone_icon" />
-            联系{order.grapher.name}
-          </a>
-        </div>
-        <div className="flex_spring"></div>
-        <div>
-          <button className="weui_btn weui_btn_mini weui_btn_default">
-            退款
-          </button>
-        </div>
-      </div>
-    </div>
-    {/* 进行中 */}
-    <div className="weui_panel_bd">
-      <div className="yp_media_box">
-        <div>
-          <a href={`tel:${order.grapher.phone}`} className="color_gray">
-            <i className="icon phone_icon" />
-            联系{order.grapher.name}
-          </a>
-        </div>
-        <div className="flex_spring"></div>
-        <div>
-          <button className="weui_btn weui_btn_mini weui_btn_default">
-            退款
-          </button>
-          <span>&nbsp;&nbsp;</span>
-          <button className="weui_btn weui_btn_mini weui_btn_primary">
-            &nbsp;&nbsp;收片&nbsp;&nbsp;
-          </button>
-        </div>
-      </div>
-    </div>
-    {/* TODO: 已完成 */}
-    {/* 已关闭 */}
-    <div className="weui_panel_bd">
-      <div className="yp_media_box">
-        <div>
-          <a href={`tel:${order.grapher.phone}`} className="color_gray">
-            <i className="icon phone_icon" />
-            联系{order.grapher.name}
-          </a>
-        </div>
-        <div className="flex_spring"></div>
-        <div>
-          <span className="color_gray">已取消订单</span>
-          <span className="color_red">退款成功</span>
-          <span className="color_green">已完成</span>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export {YPUIOrderCard as default};
