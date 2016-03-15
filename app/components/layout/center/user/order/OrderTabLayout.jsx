cfcb5feb23675dc3d92ea6aeb4e5e4ec8a2fcfed
Peter Wong <amile1860@hotmail.com>
2016-03-15 15:59:01 +0800

    增加navbar点击后的回调和订单filter信息

--- app/components/layout/center/user/order/OrderTabLayout.jsx
+++ app/components/layout/center/user/order/OrderTabLayout.jsx
@@ -3,23 +3,34 @@ import React from 'react';
 import OrderListLayout from './OrderListLayout.jsx';
 import WeuiNavbar from '../../../../UI/WeuiNavbar';
 
+import OrderActions from '../../../../../actions/OrderActions';
+
 let navList = [{
+  filterType: ['WaitingPayment'],
   text: '待付款'
 },{
+  filterType: ['WaitingReception'],
   text: '待确认'
 },{
+  filterType: ['WaitingDelivery', 'WaitingAcceptance'],
   text: '进行中'
 },{
+  filterType: ['Completed'],
   text: '已完成'
 },{
+  filterType: ['Cloesd'], // TODO: 这个拼写错误需要认真验证
   text: '已关闭'
 }];
 
 class OrderTabLayout extends React.Component{
-	getIndex(index) {
-		console.log(index);
-	}
-
+  /**
+   * 我也不知道为什么要把整个data传出来
+   * @param data
+   */
+  getIndex(data) {
+    console.log(data);
+    OrderActions.type(data.filterType);
+  }
   render() {
     return (
 	    <div className="weui_tab">
