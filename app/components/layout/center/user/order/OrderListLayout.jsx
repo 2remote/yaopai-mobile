92e6fe3333a6a6e80064ba8c03a9adc2c556576b
Peter Wong <amile1860@hotmail.com>
2016-03-15 15:58:20 +0800

    增加列表展示的filter功能

--- app/components/layout/center/user/order/OrderListLayout.jsx
+++ app/components/layout/center/user/order/OrderListLayout.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Reflux from 'reflux';
 import ReactMixin from 'react-mixin';
-import YPUIOrderCard from '../../../../UI/YPUIOrderCard.jsx';
+import _ from 'underscore';
 
+import YPUIOrderCard from '../../../../UI/YPUIOrderCard.jsx';
 import OrderStore from '../../../../../stores/OrderStore';
 import { History } from 'react-router';
 import OrderActions from '../../../../../actions/OrderActions';
@@ -10,15 +11,21 @@ import OrderActions from '../../../../../actions/OrderActions';
 class OrderListLayout extends React.Component {
   constructor() {
     super();
-    this.state = { orders: [] };
+    this.state = {
+      filterType: [],
+      orders: []
+    };
   }
   componentDidMount() {
+    // 手动为默认展示选择“待付款”栏数据
+    OrderActions.type(['WaitingPayment']);
     OrderActions.list('out');
   }
   onOrderLoad(order) {
     // TODO: if else and more
     console.log('[OrderListLayout]', '[onOrderLoad]', order);
     this.setState({
+      filterType: order.filterType,
       orders: order.orders
     });
   }
@@ -26,8 +33,10 @@ class OrderListLayout extends React.Component {
     return (
       <div>
         {
-          this.state.orders.map((order, index) =>
-          <YPUIOrderCard order={order} key={index}/>
+          this.state.orders.map((order, index) => {
+            if(!_.contains(this.state.filterType, order.State)) return;
+            return <YPUIOrderCard order={order} key={index}/>;
+          }
         )}
         <div
           style={{
