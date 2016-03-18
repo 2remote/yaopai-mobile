cac2fdffa55ae08b3f5461297f81f07ec8a0fcc7
Peter Wong <amile1860@hotmail.com>
2016-03-18 17:07:20 +0800

    增加摄影师订单管理代码

---
+++ app/components/layout/center/grapher/order/OrderDetailLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class OrderDetailLayout extends React.Component {
+  render() {
+    return (
+      <div>OrderDetailLayout</div>
+    );
+  }
+}
+
+export default OrderDetailLayout;
---
+++ app/components/layout/center/grapher/order/OrderTabLayout.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import OrderListLayout from './OrderListLayout.jsx';
+import WeuiNavbar from '../../../../UI/WeuiNavbar';
+import { OrderStatus } from '../../../../Tools';
+import OrderActions from '../../../../../actions/OrderActions';
+
+let navList = [{
+  filterType: OrderStatus.UNCONFIRMED,
+  text: '待确认'
+},{
+  filterType: OrderStatus.ONGOING,
+  text: '进行中'
+},{
+  filterType: OrderStatus.COMPLETE,
+  text: '已完成'
+},{
+  filterType: OrderStatus.CLOSED,
+  text: '已关闭'
+}];
+
+class OrderTabLayout extends React.Component {
+  /**
+   * 我也不知道为什么要把整个data传出来
+   * @param data
+   */
+  getIndex(data) {
+    OrderActions.type(data.filterType);
+  }
+  render() {
+    return (
+      <div className="weui_tab">
+        <WeuiNavbar list={navList} onClick={this.getIndex.bind(this)} />
+        <div className="weui_tab_bd">
+          <OrderListLayout location={{ pathname: this.props.location.pathname }} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OrderTabLayout;
