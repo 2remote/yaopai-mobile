37c27ffd75190a8fbd4d307309aac79eba937038
可乐 <zaxlct@foxmail.com>
2016-03-21 15:14:40 +0800

    摄影师账单列表20%

--- app/components/layout/center/grapher/purse/PurseDetailListLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class PurseDetailListLayout extends React.Component {
-  render() {
-    return <div>PurseDetailListLayout</div>;
-  }
-}
-
-export default PurseDetailListLayout;
---
+++ app/components/layout/center/grapher/purse/PurseListLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import YPUIPurseCard from '../../../../UI/YPUIPurseCard.jsx';
+
+class PurseListLayout extends React.Component {
+  render() {
+    return <div><YPUIPurseCard /></div>;
+  }
+}
+
+export default PurseListLayout;
