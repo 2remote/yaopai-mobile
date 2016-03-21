543a80b593d6d9f11aa928394ddcd26c85ef23d2
可乐 <zaxlct@foxmail.com>
2016-03-21 18:12:07 +0800

    提现/绑定支付宝 页面搭建

--- app/components/layout/center/grapher/withdraw/BindCard.jsx
+++ app/components/layout/center/grapher/withdraw/BindCard.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 
+import {Button} from 'react-weui';
+
 class BindCard extends React.Component {
   render() {
-    return <div>PurseDetailLayout</div>;
+    return (
+      <div>
+        <div className="weui_cells_title">提现</div>
+        <div className="weui_cells">
+          <div className=" weui_cell">
+            <div className="weui_cell_bd weui_cell_primary">
+              <p>可提余额</p>
+            </div>
+            <div className="weui_cell_ft color_red">￥0.00</div>
+          </div>
+
+          <div className="weui_cell weui_cells_form">
+            <div className="weui_cell_hd"><label className="yp_label_fix">金额（元）</label></div>
+            <div className="weui_cell_bd weui_cell_primary">
+              <input className="weui_input" type="number" pattern="[0-9]*" placeholder="请输入提现的金额" />
+            </div>
+          </div>
+        </div>
+
+        <footer className="footer" style={{paddingTop:40, paddingBottom:0}}>
+          <Button type="primary">确&nbsp;&nbsp;定</Button>
+        </footer>
+
+        <aside className="footer color_gray font_small text_center">
+          温馨提示：提现至您绑定的支付宝账号中，如您未绑定支付宝账号，请您先进行绑定再进行提现<br/>
+          <a className="color_green  font_medium">绑定支付宝<i className="icon youjiantou" /></a>
+        </aside>
+      </div>
+    );
   }
 }
 
--- app/components/layout/center/grapher/withdraw/withdrawDeposit.jsx
+++ app/components/layout/center/grapher/withdraw/withdrawDeposit.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 
+import {Button} from 'react-weui';
+
 class WithdrawDeposit extends React.Component {
   render() {
-    return <div>PurseDetailLayout</div>;
+    return (
+      <div>
+        <div className="weui_cells_title">绑定支付宝</div>
+        <div className="weui_cells">
+          <div className="weui_cell weui_cells_form">
+            <div className="weui_cell_bd weui_cell_primary">
+              <input className="weui_input" type="number" pattern="[0-9]*" placeholder="请输入支付宝账号" />
+            </div>
+          </div>
+
+          <div className="weui_cell">
+            <div className="weui_cell_bd weui_cell_primary">
+              <input className="weui_input" type="number" pattern="[0-9]*" placeholder="请输入手机验证码" />
+            </div>
+            <div className="weui_cell_ft">
+              <a className="color_green font_medium" href="javascript:;">获取验证码</a>
+            </div>
+          </div>
+        </div>
+
+        <footer className="footer" style={{paddingTop:40, paddingBottom:0}}>
+          <Button type="primary">提交绑定</Button>
+        </footer>
+
+        <aside className="footer color_gray font_small">
+          <p className="font_medium">注意：</p>
+          <ul>
+            <li>1.此账户为YAOPAI平台与您进行现金结算的通道此账户为YAOPAI平台与您进行现金结算的通道。</li>
+            <li>2.请确认支付宝及姓名为本人使用账户。</li>
+            <li>3.为确保您的资金安全，如需解绑账户，请联系人工客服认证。客服电话0371-65337727。
+            </li>
+          </ul>
+        </aside>
+      </div>
+    );
   }
 }
 
