9f7186d0b9d312f2f36fccac8b02fb5ebb97c9ea
可乐 <zaxlct@foxmail.com>
2016-03-21 16:12:47 +0800

    摄影师账单提示

--- app/components/layout/center/grapher/purse/PurseListLayout.jsx
+++ app/components/layout/center/grapher/purse/PurseListLayout.jsx
@@ -3,7 +3,21 @@ import YPUIPurseCard from '../../../../UI/YPUIPurseCard.jsx';
 
 class PurseListLayout extends React.Component {
   render() {
-    return <div><YPUIPurseCard /></div>;
+    return (
+      <div>
+        <YPUIPurseCard />
+
+        <aside
+          style={{
+            padding: '20px 15px 10px',
+            fontSize: '12px'
+          }}
+          className="color_gray text_center">
+          温馨提示：交易过程中如有异常<br />
+          请拨打客服热线：0371-65337727
+        </aside>
+      </div>
+    );
   }
 }
 
