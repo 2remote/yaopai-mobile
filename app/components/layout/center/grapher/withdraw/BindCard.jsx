65e3754254e49f98799233d549ae9a8b46f769fc
可乐 <zaxlct@foxmail.com>
2016-03-21 16:23:57 +0800

    新建“绑定支付宝/提现”页面

---
+++ app/components/layout/center/grapher/withdraw/BindCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class BindCard extends React.Component {
+  render() {
+    return <div>PurseDetailLayout</div>;
+  }
+}
+
+export default BindCard;
---
+++ app/components/layout/center/grapher/withdraw/withdrawDeposit.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class WithdrawDeposit extends React.Component {
+  render() {
+    return <div>PurseDetailLayout</div>;
+  }
+}
+
+export default WithdrawDeposit;
