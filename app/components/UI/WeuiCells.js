413d2bdb1123094e89207e13234a23293f14cbb1
Peter Wong <amile1860@hotmail.com>
2016-03-22 10:28:30 +0800

    增加access控制和desc字段展示

--- app/components/UI/WeuiCells.js
+++ app/components/UI/WeuiCells.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 /**
  * weui-cells的React组件
+ * @param access: bool值，决定是否显示右侧箭头,默认true
  * @param cellList: 包含该weui-cells下的每条cell数据,这些数据包含:
  *   icon: cell左侧图标,此处仅支持icon-font
  *   title: cell左侧标题
  *   money: cell右侧金额,颜色为red,单位为¥
+ *   desc: cell右侧解释文字
  *   href: cell跳转地址(React Router的地址)
  */
 class WeuiCells extends React.Component {
   render() {
     let result = this.props.cellList.map((cell, index)=>{
       return (
-        <a className="weui_cell" href={`#${cell.href}`} key={index}>
+        <a className="weui_cell" href={cell.href ? `#${cell.href}` : 'javascript:;'} key={index}>
           <div className="weui_cell_hd">
             <i className={`icon ${cell.icon}`} />&nbsp;
           </div>
@@ -23,12 +25,19 @@ class WeuiCells extends React.Component {
             {
               !cell.money || (<span style={{color:'red'}}>¥&nbsp;{cell.money}</span>)
             }
+            {
+              !cell.desc || (<span className="font_medium">{cell.desc}</span>)
+            }
           </div>
         </a>
       );
     });
+    let access = this.props.access;
+    if(access === undefined) { // 兼容以往没有传这个参数的情况
+      access = true;
+    }
     return(
-      <div className="weui_cells weui_cells_access">
+      <div className={`weui_cells${access?' weui_cells_access':''}`}>
         {result}
       </div>
     );
