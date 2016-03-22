7c546f04c7d3b62081aaf7be1d4daeec9547cc50
可乐 <zaxlct@foxmail.com>
2016-03-22 23:09:48 +0800

    接入数据

--- app/actions/UserFundActions.js
+++ app/actions/UserFundActions.js
@@ -3,7 +3,8 @@ import HttpFactory from '../HttpFactory';
 import API from '../api';
 
 var UserFundActions = Reflux.createActions({
-  'currentAccount': {children: ['success', 'failed']}
+  'currentAccount': {children: ['success', 'failed']},
+  'recordsSearch': {children: ['success', 'failed']}
 });
 
 UserFundActions.currentAccount.listen(function() {
@@ -11,6 +12,13 @@ UserFundActions.currentAccount.listen(function() {
     Fields: 'Id,Available,Frozen,TotalRevenue,Receivable'
   };
   HttpFactory.post(API.USERFUND.currentAccount,data,this.success,this.failed);
+});
+
+UserFundActions.recordsSearch.listen(function() {
+  var data = {
+    Fields: 'Id,Amount,CreationTime,AssociatedId,FundsType'
+  };
+  HttpFactory.post(API.USERFUND.recordsSearch,data,this.success,this.failed);
 });
 
 export {UserFundActions as default};
--- app/stores/UserFundStore.js
+++ app/stores/UserFundStore.js
@@ -6,24 +6,35 @@ var UserFundStore = Reflux.createStore({
   init: function() {
     // Id,Available,Frozen,TotalRevenue,Receivable,ErrorCode,ErrorMsg,Success
     this.data = {
-      userId: '', // 用户ID
-      Available: '', // 可提现余额
-      Frozen : '', // 被冻结的金额(尚在提现中的资金会被冻结)
-      TotalRevenue: '', // //收入总计
-      Receivable: '', // 收款帐号
-      ErrorCode: '',
-      hintMessage: '',
-      success: false
+      purse:{//摄影师钱包
+        userId: '', // 用户ID
+        Available: '', // 可提现余额
+        Frozen : '', // 被冻结的金额(尚在提现中的资金会被冻结)
+        TotalRevenue: '', // //收入总计
+        Receivable: '', // 收款帐号
+      },
+      list: [//摄影师账单流水
+        {}
+      ],
+      hintMessage : '',
+      success : false,
+      flag : '',
+      filterType: ''
     };
     this.listenTo(UserFundActions.currentAccount.success, this.onCurrentAccountSuccess);
     this.listenTo(UserFundActions.currentAccount.failed, this.onFailed);
+
+    this.listenTo(UserFundActions.recordsSearch.success, this.onRecordsSearchSuccess);
+    this.listenTo(UserFundActions.recordsSearch.failed, this.onFailed);
   },
   _fillData: function(data) {
-    this.data.userId = data.Id;
-    this.data.Available = data.Available;
-    this.data.Frozen = data.Frozen;
-    this.data.TotalRevenue = data.TotalRevenue;
-    this.data.Receivable = data.Receivable;
+    this.data.purse.userId = data.Id;
+    this.data.purse.Available = data.Available;
+    this.data.purse.Frozen = data.Frozen;
+    this.data.purse.TotalRevenue = data.TotalRevenue;
+    this.data.purse.Receivable = data.Receivable;
+
+    this.data.list = data.Result;
   },
   onCurrentAccountSuccess: function (data) {
     this.data.flag = 'resetPassword';
@@ -37,6 +48,21 @@ var UserFundStore = Reflux.createStore({
     }
     this.trigger(this.data);
   },
+
+  onRecordsSearchSuccess: function(data) {
+    //console.log(data);
+    this.data.flag = 'resetPassword';
+    if (data.Success) {
+      this.data.hintMessage = '';
+      this.data.success = true;
+      this._fillData(data);
+    } else {
+      this.data.hintMessage = data.ErrorMsg;
+      this.data.success = false;
+    }
+    this.trigger(this.data);
+  },
+
   onFailed: function(data) {
     this.data.hintMessage = "网络出错啦！";
     this.trigger(this.data);
