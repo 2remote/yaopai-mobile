b85279d647d2b8372bbcf2108866eb3ae3dfda07
Peter Wong <amile1860@hotmail.com>
2016-03-22 10:28:02 +0800

    增加我的钱包跳转

--- app/components/UserCenterPage/GrapherCenterPage.js
+++ app/components/UserCenterPage/GrapherCenterPage.js
@@ -47,6 +47,7 @@ var GrapherCenterPage = React.createClass({
         <UserAvatarBox background={true} data={this.state.userInfo}/>
         {/*makeIconButton('order_icon', '订单管理', 'grapher_tickets', 'react-router')*/}
         {makeIconButton('order_icon', '订单管理', 'center/g/order', 'react-router')}
+        {makeIconButton('wallet_icon', '我的钱包', 'center/g/purse', 'react-router')}
         {makeIconButton('home_icon', '我的主页', 'grapherDetail/'+ this.state.userInfo.userId, 'react-router')}
         {makeIconButton('customer_icon', '联系客服', 'tel:+86-0371-6533-7727')}  
       </div>
