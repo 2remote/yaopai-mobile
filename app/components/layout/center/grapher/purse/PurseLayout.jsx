0faaa96ebcf4934a264aa50f80d019e2ff07499e
Peter Wong <amile1860@hotmail.com>
2016-03-22 10:28:51 +0800

    复用已有UI组件；接入数据

--- app/components/layout/center/grapher/purse/PurseLayout.jsx
+++ app/components/layout/center/grapher/purse/PurseLayout.jsx
@@ -1,45 +1,76 @@
 import React from 'react';
+import Reflux from 'reflux';
+import ReactMixin from 'react-mixin';
+import WeuiCells from '../../../../UI/WeuiCells';
+import UserAvatarBox from '../../../../UserAvatarBox' ;
+
+import UserActions from '../../../../../actions/UserActions';
+import UserStore from '../../../../../stores/UserStore';
+import UserFundActions from '../../../../../actions/UserFundActions';
+import UserFundStore from '../../../../../stores/UserFundStore';
+
+let operationList = [{
+  icon: 'order_icon',
+  title: '账单',
+  href: '/center/g/purse/detail'
+}, {
+  icon: 'wallet_icon',
+  title: '提现',
+  href: '/center/g/purse/withdraw'
+}];
 
 class PurseLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balanceList: [{
+        title: '账户余额',
+        money: ''
+      }],
+      bindOpList: [{
+        icon: 'zhifubao',
+        title: '绑定支付宝',
+        desc: '未绑定支付宝',
+        href: '/center/g/purse/bind'
+      }],
+      user: {},
+      fundData: {}
+    };
+  }
+  componentDidMount() {
+    UserActions.currentUser();
+  }
+  onUserLoad(user) {
+    if(!user.isLogin){ // 用户未登录，跳转登陆页
+      this.history.pushState({netxPage : this.props.location.pathname},'/login_page');
+    } else {
+      this.setState({ user });
+      // 获取用户fund信息
+      UserFundActions.currentAccount();
+    }
+  }
+  onUserFundLoad(fundData) {
+    this.setState({
+      fundData,
+      balanceList: [{
+        title: '账户余额',
+        money: fundData.Available
+      }],
+      bindOpList: [{
+        icon: 'zhifubao',
+        title: '绑定支付宝',
+        href: '/center/g/purse/bind',
+        desc: `${fundData.Receivable?'已绑定支付宝':'未绑定支付宝'}`
+      }]
+    });
+  }
   render() {
-    let ceshi = "http://7xrgj5.com1.z0.glb.clouddn.com/35/009098bc-6d59-4443-93ec-3e9f4d5bb277.jpg?imageMogr2/auto-orient/gravity/Center/thumbnail/!78x78r/crop/78x78/interface/1";
     return (
       <div>
-        <header><img src={ceshi} alt=""/></header>
-
-        <section className="weui_cells">
-          <div className="weui_cell">
-            <div className="weui_cell_bd weui_cell_primary">
-              <p>账户余额</p>
-            </div>
-            <div className="weui_cell_ft color_red">￥0.00</div>
-          </div>
-        </section>
-
-        <section className="weui_cells weui_cells_access">
-          <a className="weui_cell" href="javascript:;">
-            <div className="weui_cell_bd weui_cell_primary">
-              <p><i className="icon order_icon" />&nbsp;账单</p>
-            </div>
-            <div className="weui_cell_ft"></div>
-          </a>
-          <a className="weui_cell" href="javascript:;">
-            <div className="weui_cell_bd weui_cell_primary">
-              <p><i className="icon wallet_icon" />&nbsp;提现</p>
-            </div>
-            <div className="weui_cell_ft"></div>
-          </a>
-        </section>
-
-        <section className="weui_cells weui_cells_access">
-          <a className="weui_cell" href="javascript:;">
-            <div className="weui_cell_bd weui_cell_primary">
-              <p><i className="icon zhifubao" />&nbsp;绑定支付宝</p>
-            </div>
-            <div className="weui_cell_ft"><small className="font_small">未绑定支付宝</small></div>
-          </a>
-        </section>
-
+        <UserAvatarBox background={true} data={this.state.user}/>
+        <WeuiCells cellList={this.state.balanceList} access={false} />
+        <WeuiCells cellList={operationList} />
+        <WeuiCells cellList={this.state.bindOpList} />
         <aside className="color_gray font_small footer">
           绑定支付宝账号后，即可提现。如充值或提现遇到特殊情况，请联系客服，或拨打0371-65337727帮助
         </aside>
@@ -47,5 +78,8 @@ class PurseLayout extends React.Component {
     );
   }
 }
+
+ReactMixin.onClass(PurseLayout, Reflux.listenTo(UserStore, 'onUserLoad'));
+ReactMixin.onClass(PurseLayout, Reflux.listenTo(UserFundStore, 'onUserFundLoad'));
 
 export default PurseLayout;
