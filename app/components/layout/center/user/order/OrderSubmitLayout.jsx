825a678a95f3c423e606de41e7ce6bc47b3fb47d
Peter Wong <amile1860@hotmail.com>
2016-03-29 14:31:45 +0800

    调整微信支付的逻辑

--- app/components/layout/center/user/order/OrderSubmitLayout.jsx
+++ app/components/layout/center/user/order/OrderSubmitLayout.jsx
@@ -3,13 +3,15 @@ import ReactMixin from 'react-mixin';
 import Reflux from 'reflux';
 import { History } from 'react-router';
 import WeUI from 'react-weui';
+import $ from 'jquery';
+import API from '../../../../../api';
 
 import UserActions from '../../../../../actions/UserActions';
 import OrderActions from '../../../../../actions/OrderActions';
 import UserStore from '../../../../../stores/UserStore';
 import OrderStore from '../../../../../stores/OrderStore';
 
-const {Cells, CellsTitle, CellsTips, Cell, CellHeader, CellBody, CellFooter, Button} = WeUI;
+const { CellsTitle } = WeUI;
 
 function getCookie(cname) {
   var name = cname + "=";
@@ -29,47 +31,65 @@ class OrderSubmitLayout extends React.Component {
   //TODO: 点击提交订单后的事件
   submit = (e) => {
     e.preventDefault();
-    pingpp_one.init({
-      debug: true,
-      app_id: 'app_HOmP4CHinvvL9Kyv', //Ping++ 后台中的应用Id
-      amount: 0,    //金额请填写0
-      channel: ['alipay_wap', 'wx_pub', 'upacp_wap'],//渠道数组,视情况而定
-      charge_url: `http://dev.api.aiyaopai.com/payment/token?tokenId=${getCookie('pingToken')}`, //token地址
-      open_id:'',
-      charge_param: {
-        'callback': `#/center/u/order/${this.state.order.Id}/submit/result`,
-        'orderId': this.state.order.Id
-      }  //订单Id
-    }, res => {
-      if(res.debug&&res.chargeUrlOutput){
-        console.log(res.chargeUrlOutput);
+    let self = this;
+    let pingppPay = (self, openid) => {
+      let initData = {
+        debug: true,
+        app_id: 'app_HOmP4CHinvvL9Kyv', //Ping++ 后台中的应用Id
+        amount: 0,    //金额请填写0
+        channel: ['alipay_wap', 'wx_pub', 'upacp_wap'],//渠道数组,视情况而定
+        charge_url: `http://dev.api.aiyaopai.com/payment/token?tokenId=${getCookie('pingToken')}`, //token地址
+        charge_param: {
+          'callback': `#/center/u/order/${self.state.order.Id}/submit/result`,
+          'orderId': self.state.order.Id
+        }  //订单Id
+      };
+      if(openid) {
+        initData.open_id = openid;
       }
-      if(!res.status){
-        //处理错误
-        alert(res.msg);
-      }
-      else{
-        //debug 模式下调用 charge_url 后会暂停，可以调用 pingpp_one.resume 方法继续执行
-        if(res.debug&&!res.wxSuccess){
-          if(confirm('当前为 debug 模式，是否继续支付？')){
-            pingpp_one.resume();
+      pingpp_one.init(initData, res => {
+        if(res.debug&&res.chargeUrlOutput){
+          //alert('charge ' + res.chargeUrlOutput);
+        }
+        if(!res.status) { // 付款失败，处理错误
+          alert('付款失败，请稍后再试');
+        } else { // 付款成功
+          //debug 模式下调用 charge_url 后会暂停，可以调用 pingpp_one.resume 方法继续执行
+          if(res.debug&&!res.wxSuccess){
+            if(confirm('当前为 debug 模式，是否继续支付？')){
+              pingpp_one.resume();
+            }
+          } else {
+            window.location.href = `http://dev.api.aiyaopai.com/payment/successful?orderId=${self.state.order.Id}`;
           }
         }
-        //若微信公众号渠道需要使用壹收款的支付成功页面，则在这里进行成功回调，
-        //调用 pingpp_one.success 方法，你也可以自己定义回调函数
-        //其他渠道的处理方法请见第 2 节
-        else pingpp_one.success(function(res){
-          if(!res.status){
-            alert(res.msg);
+      });
+    };
+    if(WeChat) {//WeChat) { // 在微信内部
+      $.ajax({
+        url : API.USERFUND.weixinAuthGet,
+        type : 'POST',
+        dataType : 'json',
+        timeout : 5000,
+        crossDomain : true,
+        xhrFields:{
+          withCredentials : true
+        },
+        success: function(data) {
+          if(data.Success && data.Result){
+            pingppPay(self, data.Result);
+          } else {
+            // 未能获取openid
+            pingppPay(self);
           }
-        },function(){
-          //这里处理支付成功页面点击“继续购物”按钮触发的方法，
-          //例如：若你需要点击“继续购物”按钮跳转到你的购买页，
-          //则在该方法内写入 window.location.href = "你的购买页面 url"
-          window.location.href='http://yourdomain.com/payment_succeeded';//示例
-        });
-      }
-    });
+        },
+        error: function() {
+          pingppPay(self);
+        }
+      });
+    } else {
+      pingppPay(self);
+    }
   };
 
   constructor(props) {
