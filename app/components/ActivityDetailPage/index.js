1f83340e461dd46d7d3bdb3b34ac15de08225ecd
Haitao <liubiantao@gmail.com>
2016-04-15 14:14:06 +0800

    marigin => margin

--- app/components/ActivityDetailPage/index.js
+++ app/components/ActivityDetailPage/index.js
@@ -20,7 +20,7 @@ var ActivityDetailPage = React.createClass({
       data:[]
     };
   },
-  
+
   componentDidMount: function() {
     if(!this.props.params.Id)
       return this.history.pushState(null,'/Activity');
@@ -39,14 +39,14 @@ var ActivityDetailPage = React.createClass({
     const negotiable = this.state.data.Negotiable;
     const price = this.state.data.Price;
     let priceResult = "面议";
-    
+
     if(!negotiable){
       priceResult = price;
     }
     return (
-    <span 
+    <span
       style={{color:'#050505',fontWeight:'bold'}}
-      ref="workPrice" 
+      ref="workPrice"
       className="workPrice">{priceResult}</span>
     )
   },
@@ -69,7 +69,7 @@ var ActivityDetailPage = React.createClass({
         <DocumentTitle title={this.state.data.Title + TITLE.activityDetailPage} />
 
         <div style={{margin:'56px 20% 0 11%'}}>
-          <span style={{color:'green',float:'left',fontSize:'2em',mariginRight:3}}>-</span>
+          <span style={{color:'green',float:'left',fontSize:'2em',marginRight:3}}>-</span>
           <h1>{this.state.data.Title}</h1>
         </div>
 
@@ -79,12 +79,12 @@ var ActivityDetailPage = React.createClass({
           srcSet="imgs/workDetailPage/work-split-line@2X.png 2x" />
 
         <div className="ActivityCover" >
-          <img 
+          <img
             style={{width:'100%',height:210/375*innerWidth, marginBottom: 15}}
             src={cover} />
         </div>
 
-        <div 
+        <div
           className="ActivityContent"
           dangerouslySetInnerHTML={makeContent()} />
 
@@ -93,7 +93,7 @@ var ActivityDetailPage = React.createClass({
           style={{width: '70%', marginLeft: '15%'}}
           src="imgs/workDetailPage/work-split-line.png"
           srcSet="imgs/workDetailPage/work-split-line@2X.png 2x" />
-          
+
         <Share />
         <WechatShare title={wechatShareTitle} desc={wechatShareDesc}>
         </WechatShare>
