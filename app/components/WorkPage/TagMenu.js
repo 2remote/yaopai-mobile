1d1f401e5ef1b609c5689e44f0add7a45fdcc702
可乐 <zaxlct@foxmail.com>
2016-06-10 19:37:17 +0800

    重构 SearchRow 减少 props 传递层级

--- app/components/WorkPage/TagMenu.js
+++ app/components/WorkPage/TagMenu.js
@@ -1,32 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
 
-class SearchRow extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: ""
-    }
-  }
-  handleChange(event) {
-    this.setState({value: event.target.value})
-  }
-  handleClick() {
-    this.props.onSearch(this.state.value)
-  }
-  render() {
-    return (
-      <div>
-        <input type="text"
-          ref
-          value={this.state.value}
-          onChange={this.handleChange.bind(this)} />
-        <button onClick={this.handleClick.bind(this)}>搜索</button>
-      </div>
-    )
-  }
-}
-
 var TagCol = React.createClass({
   handleClick() {
     $("#tagColBox").toggleClass('tagColBoxActive');
@@ -69,21 +43,38 @@ var TagRow = React.createClass({
   }
 });
 
-var TagMenu = React.createClass({
+class TagMenu extends React.Component{
   toggle() {
     $("#tagMenu").slideToggle();
-  },
+  }
 
   render() {
+    let searchText;
     return (
-      <div className="tagMenu" id="tagMenu">
-        <SearchRow onSearch = {this.props.onSearch}/>
+      <section className="tagMenu" id="tagMenu">
+        <div>
+          <input
+            type="text"
+            placeholder="找不到想要的？试着搜一下！"
+            ref="search"
+          />
+          { /* button 改成 icon */ }
+          <button onClick={event => {
+            event.preventDefault();
+            let text = this.refs.search.value.trim();
+            if (text) this.props.onSearch(text)
+          }}>
+            搜索
+          </button>
+        </div>
+
         <span>拍摄地区 | Shooting Area</span>
         <TagRow data={this.props.cities} onSelectedTag={this.props.onSelectedTag}/>
         <span>拍摄种类 | Shooting Type</span>
         <TagRow data={this.props.catas} onSelectedTag={this.props.onSelectedTag}/>
-      </div>
+      </section>
     );
   }
-});
-export {TagMenu as default};
+};
+
+export default TagMenu;
