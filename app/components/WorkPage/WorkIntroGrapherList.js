9dd76bd4b1abb95e3fb4cede50710eb041f881a7
可乐 <zaxlct@foxmail.com>
2016-06-11 11:34:44 +0800

    改成函数式组件

--- app/components/WorkPage/WorkIntroGrapherList.js
+++ app/components/WorkPage/WorkIntroGrapherList.js
@@ -1,22 +1,17 @@
 import React from 'react';
-
 import WorkIntroGrapherRow from './WorkIntroGrapherRow';
 
-var WorkIntroGrapherList = React.createClass({
-  render() {
-    return (
-      <div className="workIntroGrapherList" style={{marginTop:49}}>
-        {
-          this.props.data.map((work, i) =>
-            <WorkIntroGrapherRow
-              key={i}
-              data={work}
-            />
-          )
-        }
-      </div>
-    );
-  }
-});
+const WorkIntroGrapherList = ({data}) => (
+  <div className="workIntroGrapherList">
+    {
+      data.map((work, i) =>
+        <WorkIntroGrapherRow
+          key={i}
+          data={work}
+        />
+      )
+    }
+  </div>
+);
 
-export {WorkIntroGrapherList as default};
+export default WorkIntroGrapherList;
--- app/components/WorkPage/WorkIntroGrapherRow.js
+++ app/components/WorkPage/WorkIntroGrapherRow.js
@@ -4,48 +4,46 @@ import { Link } from 'react-router';
 import {imgModifier} from '../Tools';
 import LazyLoad from 'react-lazy-load';
 
-var WorkIntroGrapherRow = React.createClass({
-  render() {
-    let cover;
-    if(this.props.data.Cut){
-      var cut = JSON.parse(this.props.data.Cut);
-      cover = this.props.data.Cover + cut.w;
-    }else{
-      cover = imgModifier(this.props.data.Cover, "workCover");
-    }
-    const avatar = imgModifier(this.props.data.Photographer.Avatar, "avatar");
-    return (
-      <div className="workIntroGrapherRow">
-        <Link to={"/workDetail/" + this.props.data.Id}>
-          <div className="card-work" style={{height: 254/375*innerWidth}}>
-
-            <LazyLoad threshold={100} once>
-              <div className="card-price">
-                ￥{this.props.data.Price} <span className="font_small">/套</span>
-                <div className="triangle-top-left"></div>
-                <div className="triangle-bottom-left"></div>
-              </div>
-
-              <img
-                style={{width:'100%',height:254/375*innerWidth}}
-                src={cover}
-              />
-            </LazyLoad>
-
-          </div>
-        </Link>
-
-        <a className="card-head-face" href={`/grapherDetail/${this.props.data.UserId}`}>
-          <img src={avatar} />
-        </a>
-
-        <div className="card-info">
-          <p className="info-title">{this.props.data.Title}</p>
-          <p className="info-Subtitle">有{this.props.data.Views}人想拍</p>
-        </div>
-      </div>
-    );
+const WorkIntroGrapherRow = ({data}) => {
+  let cover;
+  if(data.Cut){
+    const cut = JSON.parse(data.Cut);
+    cover = data.Cover + cut.w;
+  }else{
+    cover = imgModifier(data.Cover, "workCover");
   }
-});
+  const avatar = imgModifier(data.Photographer.Avatar, "avatar");
 
-export {WorkIntroGrapherRow as default};
+  return (
+    <div className="workIntroGrapherRow">
+      <Link to={`/workDetail/${data.Id}`}>
+        <div className="card-work" style={{height: 254/375*innerWidth}}>
+          <LazyLoad threshold={100} once>
+            <div className="card-price">
+              ￥{data.Price} <span className="font_small">/套</span>
+              <div className="triangle-top-left"></div>
+              <div className="triangle-bottom-left"></div>
+            </div>
+
+            <img
+              style={{width:'100%',height:254/375*innerWidth}}
+              src={cover}
+            />
+          </LazyLoad>
+
+        </div>
+      </Link>
+
+      <a className="card-head-face" href={`/grapherDetail/${data.UserId}`}>
+        <img src={avatar} />
+      </a>
+
+      <div className="card-info">
+        <p className="info-title">{data.Title}</p>
+        <p className="info-Subtitle">有{data.Views}人想拍</p>
+      </div>
+    </div>
+  );
+}
+
+export default WorkIntroGrapherRow;
