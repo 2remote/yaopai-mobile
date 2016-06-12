d60e0a1c4ece16d0dd311457d1eaeeea642bbfec
可乐 <zaxlct@foxmail.com>
2016-06-12 17:51:53 +0800

    重写筛选上下拉动画

--- app/components/WorkPage/ShowMenu.js
+++ app/components/WorkPage/ShowMenu.js
@@ -3,7 +3,7 @@ import TagRow from './TagRow';
 import $ from 'jquery';
 
 const ShowMenu = ({onSearch, cities, catas, onSelectedTag}) => {
-  const handleClick = () => $("#tagMenu").slideToggle();
+  const handleClick = () => $("#tagMenu").toggleClass('tagmenu-toggle');
   let searchText;
   return (
     <section className="tagBox">
