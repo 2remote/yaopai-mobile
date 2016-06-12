ad727853646299851c6c1c733e8af76cfb0fea82
slfbest@163.com <slfbest@163.com>
2016-06-12 14:37:03 +0800

    调整摄影师查询接口

--- app/actions/AlbumsActions.js
+++ app/actions/AlbumsActions.js
@@ -12,8 +12,21 @@ var AlbumsActions = Reflux.createActions({
   'getMyAlbums' : {children : ['success','failed']},
   'onSale' : {children:['success','failed']},
   'offSale' : {children:['success','failed']},
-  'getTagList' : {children:['success','failed']}
+  'getTagList' : {children:['success','failed']},
+  'getById' : {children:['success','failed']},
 });
+
+/*
+ 根据摄影师Id查询摄影师列表
+ */
+AlbumsActions.getById.listen(function(id){
+  var data = {
+    UserId : id,
+    Fields : 'Id,Result.Title,Service,Cut',
+  };
+  HttpFactory.post(API.ALBUMS.getById,data,this.success,this.failed);
+});
+
 
 AlbumsActions.get.listen(function(id){
   var data = {
@@ -31,12 +44,13 @@ AlbumsActions.get.listen(function(id){
 AlbumsActions.search.listen( aaSearch );
 AlbumsActions.searchByTags.listen( aaSearch );
 
-function aaSearch (categoryId = null ,pageIndex = 1 ,pageSize = 10, tags=null){
+function aaSearch (categoryId = null ,pageIndex = 1 ,pageSize = 10, tags=null, creationTimeDesc=true){
   var data = {
     PageIndex:pageIndex,
     PageSize:pageSize,
     CategoryId : categoryId,
     Tags: tags,
+    CreationTimeDesc:creationTimeDesc,
     Fields : 'Id,Title,UserId,CategoryId,Description,Service,Price,' +
     'Cover,Cut,Photos.Id,Photos.AlbumsId,Photos.Url,Photos.Description,' +
     'User.Id,Photographer.NickName,Photographer.Avatar,Views,Price,' +
