27183078f36271c9ccc3d6d83c08864c41ba0756
可乐 <zaxlct@foxmail.com>
2016-06-13 14:32:52 +0800

    Revert "删删删删删删删删删删删删"
    This reverts commit a5f90635fad53ef0fad07c426530c4bbce3dc21f.

---
+++ app/components/BookPage/BookModify.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import DocumentTitle from 'react-document-title';
+import SidePage from '../UI/SidePage';
+import BookIntro from './WorkBookIntro';
+import BookForm from './WorkBookForm';
+
+
+var BookPage = React.createClass({
+  render: function() {
+    return (
+      <DocumentTitle title="作品预约">
+        <div
+          style={{
+            textAlign: 'center',
+            position: 'absolute',
+            width: '100%',
+            minHeight: '100%',
+            backgroundImage:'url(imgs/bookPageBg.png)'
+          }}
+          className="bookPage">
+          <SidePage />
+          <BookIntro />
+          <BookForm subValue="修改预约" />
+        </div>
+      </DocumentTitle>
+    );
+  }
+});
+
+export {BookPage as default};
