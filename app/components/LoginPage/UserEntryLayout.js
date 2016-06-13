96233bd45d0c15957e3ef5b9e4c9ef8255f6f7c4
可乐 <zaxlct@foxmail.com>
2016-06-13 20:09:34 +0800

    样式微调

--- app/components/LoginPage/UserEntryLayout.js
+++ app/components/LoginPage/UserEntryLayout.js
@@ -6,7 +6,7 @@ const UserEntryLayout = () => (
   <section className="entry-container" style={{height: windowHeight * 0.4 + 'px'}}>
     <header className="entry-header">
       <i className="icon yaopainew"></i>
-      <p style={{color:'#000'}}>全球&nbsp;预约&nbsp;摄影师&nbsp;平台</p>
+      <p>全球&nbsp;预约&nbsp;摄影师&nbsp;平台</p>
     </header>
     <nav className="entry-nav">
       <Link to="/login_page" activeClassName="active">登录 | Login</Link>
