e1b26fb4cb597e2ab250ced710ad4c8304bbf3e0
可乐 <zaxlct@foxmail.com>
2016-06-14 11:47:47 +0800

    更改逻辑
    1. 把个人作品数（data.TotalAlbums）为 0 的摄影师剔除掉
    2. underscore 文档不太熟，用力一个 _.partition() 方法…… @liubiantao

--- app/components/GrapherPage/GrapherList.js
+++ app/components/GrapherPage/GrapherList.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import GrapherRow from './GrapherRow';
+import _ from 'underscore';
 
-const GrapherList = ({data}) => (
-  <div className="grapherList">
-    {
-      data.map((work, i) =>
-        <GrapherRow
-          key={i}
-          data={work}
-        />
-      )
-    }
-  </div>
-);
+const GrapherList = ({data}) => {
+  // 把个人作品数（data.TotalAlbums）为 0 的摄影师剔除掉
+  const dataSift = (_.partition(data, (data) => data.TotalAlbums == 0))[1]
+  return (
+    <div className="grapherList">
+      {
+        dataSift.map((work, i) =>
+          <GrapherRow
+            key={i}
+            data={work}
+          />
+        )
+      }
+    </div>
+  )
+};
 
 export {GrapherList as default};
--- app/components/GrapherPage/GrapherRow.js
+++ app/components/GrapherPage/GrapherRow.js
@@ -6,7 +6,6 @@ import {imgModifier} from '../Tools';
 
 import PhotographerAlbumPoolAction from '../../actions/PhotographerAlbumPoolAction';
 import PhotographerAlbumPoolStore from '../../stores/PhotographerAlbumPoolStore';
-import $ from 'jquery';
 
 class GrapherRow extends React.Component {
   constructor(props){
@@ -29,57 +28,47 @@ class GrapherRow extends React.Component {
     }
   }
   render() {
-    console.log(this.state)
-
     const rondomAvatar = '//user.file.aiyaopai.com/_randomAvatar/' + (parseInt(this.props.data.User.Id) % 47 + 1 ) + '.png';
     const grapherAvatar = imgModifier(this.props.data.Avatar||rondomAvatar, "grapherAvatar");
     let pool = this.state.photographerAlbumPool;
     let currentId = this.props.data.Id;
     let covers = this.state.photographerAlbumPool[currentId] || [];
-    if(this.props.data.TotalAlbums != 0){ // 摄影师个人作品总数
-      console.log(covers[0]);
-
-      return (
-        <div className="grapherRow">
-          <Link to={"/grapherDetail/"+this.props.data.Id} >
-            <div className="info">
-              <div className="avatar" style={{backgroundImage:`url(${grapherAvatar})`}} />
-              <div className="grapher">
-                <p>
-                  <span className="title">{this.props.data.NickName?this.props.data.NickName:'还没想好名字'}</span>
-                  <span className="dest"><i className="icon didian"></i>{this.props.data.CityName}</span>
-                </p>
-                <p className="sign">{this.props.data.Signature}</p>
-              </div>
+    return (
+      <div className="grapherRow">
+        <Link to={"/grapherDetail/"+this.props.data.Id} >
+          <div className="info">
+            <div className="avatar" style={{backgroundImage:`url(${grapherAvatar})`}} />
+            <div className="grapher">
+              <p>
+                <span className="title">{this.props.data.NickName?this.props.data.NickName:'还没想好名字'}</span>
+                <span className="dest"><i className="icon didian"></i>{this.props.data.CityName}</span>
+              </p>
+              <p className="sign">{this.props.data.Signature}</p>
             </div>
-            <div className="detail">
-              <ul>
-                <li>
-                  <i className="icon grid"></i>{this.props.data.TotalAlbums}套作品
-                </li>
-                <li>
-                  <i className="icon dingdan" style={{fontSize:'13px'}}></i> {this.props.data.Sales}个订单
-                </li>
-                <li>
-                  <i className="icon xiazai11" style={{fontSize:'14px'}}></i> {this.props.data.Marks}个关注
-                </li>
-              </ul>
-            </div>
-            <div className="pics">
-              <ul>
-                <li className="cover" style={{backgroundImage:`url(${covers[0]?covers[0].Cover:'/app/imgs/grapherPage/yaopai-bg.png'})`}}></li>
-                <li className="cover" style={{backgroundImage:`url(${covers[1]?covers[1].Cover:'/app/imgs/grapherPage/yaopai-bg.png'})`}}></li>
-                <li className="cover" style={{backgroundImage:`url(${covers[2]?covers[2].Cover:'/app/imgs/grapherPage/yaopai-bg.png'})`}}></li>
-              </ul>
-            </div>
-          </Link>
-        </div>
-      );
-    } else {
-      return (
-        <div></div>
-      );
-    }
+          </div>
+          <div className="detail">
+            <ul>
+              <li>
+                <i className="icon grid"></i>{this.props.data.TotalAlbums}套作品
+              </li>
+              <li>
+                <i className="icon dingdan" style={{fontSize:'13px'}}></i> {this.props.data.Sales}个订单
+              </li>
+              <li>
+                <i className="icon xiazai11" style={{fontSize:'14px'}}></i> {this.props.data.Marks}个关注
+              </li>
+            </ul>
+          </div>
+          <div className="pics">
+            <ul>
+              <li className="cover" style={{backgroundImage:`url(${covers[0]?covers[0].Cover:'/app/imgs/grapherPage/yaopai-bg.png'})`}}></li>
+              <li className="cover" style={{backgroundImage:`url(${covers[1]?covers[1].Cover:'/app/imgs/grapherPage/yaopai-bg.png'})`}}></li>
+              <li className="cover" style={{backgroundImage:`url(${covers[2]?covers[2].Cover:'/app/imgs/grapherPage/yaopai-bg.png'})`}}></li>
+            </ul>
+          </div>
+        </Link>
+      </div>
+    );
   }
 };
 
