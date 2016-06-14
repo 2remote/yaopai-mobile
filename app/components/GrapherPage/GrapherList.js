375e593e2c75077e69f31faac7c8da3fd2c85560
可乐 <zaxlct@foxmail.com>
2016-06-14 22:26:35 +0800

    更改 JSON 数据筛选方法

--- app/components/GrapherPage/GrapherList.js
+++ app/components/GrapherPage/GrapherList.js
@@ -4,11 +4,11 @@ import _ from 'underscore';
 
 const GrapherList = ({data}) => {
   // 把个人作品数（data.TotalAlbums）为 0 的摄影师剔除掉
-  const dataSift = (_.partition(data, (data) => data.TotalAlbums == 0))[1]
+  const dataFilter = _.filter(data, data => data.TotalAlbums !== 0)
   return (
     <div className="grapherList">
       {
-        dataSift.map((work, i) =>
+        dataFilter.map((work, i) =>
           <GrapherRow
             key={i}
             data={work}
