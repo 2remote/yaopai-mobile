3ba609115dd1088b54ca09d025084762d138f7f4
可乐 <zaxlct@foxmail.com>
2016-06-14 11:01:23 +0800

    ES5 => ES6

--- app/components/GrapherPage/GrapherRow.js
+++ app/components/GrapherPage/GrapherRow.js
@@ -1,54 +1,42 @@
 import React from 'react';
 import Reflux from 'reflux';
-import { Router, Route, Link } from 'react-router';
-
+import ReactMixin from 'react-mixin';
+import { Link } from 'react-router';
 import {imgModifier} from '../Tools';
-import LazyLoad from 'react-lazy-load';
 
 import PhotographerAlbumPoolAction from '../../actions/PhotographerAlbumPoolAction';
 import PhotographerAlbumPoolStore from '../../stores/PhotographerAlbumPoolStore';
-
-import { API_URL } from '../../api';
 import $ from 'jquery';
 
-var deviceWidth = parseInt(window.innerWidth);
+class GrapherRow extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      photographerAlbumPool : {},
+    }
+  }
 
-var GrapherRow = React.createClass({
-  mixins: [Reflux.connect(PhotographerAlbumPoolStore, 'photographerAlbumPool')],
-  //onPhotographerStoreChange: function(data) {
+  photographerAlbumPool(photographerAlbumPool) {
+    this.setState({ photographerAlbumPool })
+  }
 
-  //},
-  //onPhotographerAlbumPool: function(pool) {
-  //  this.setState({
-  //    photographerAlbumPool: pool,
-  //  });
-  //},
-  getDefaultProps: function() {
-    return {
-      data: {
-
-      }
-    };
-  },
-  getInitialState: function() {
-    return {};
-  },
-
-  componentWillMount : function() {
+  componentWillMount() {
     if(!this.state.photographerAlbumPool[this.props.data.Id] && !this.state.already) {
       this.setState({
         already: true,
       });
       PhotographerAlbumPoolAction.update(this.props.data.Id);
     }
-  },
-  render: function() {
+  }
+  render() {
+    console.log(this.state)
+
     const rondomAvatar = '//user.file.aiyaopai.com/_randomAvatar/' + (parseInt(this.props.data.User.Id) % 47 + 1 ) + '.png';
     const grapherAvatar = imgModifier(this.props.data.Avatar||rondomAvatar, "grapherAvatar");
     let pool = this.state.photographerAlbumPool;
     let currentId = this.props.data.Id;
     let covers = this.state.photographerAlbumPool[currentId] || [];
-    if(this.props.data.TotalAlbums != 0){
+    if(this.props.data.TotalAlbums != 0){ // 摄影师个人作品总数
       console.log(covers[0]);
 
       return (
@@ -89,10 +77,12 @@ var GrapherRow = React.createClass({
       );
     } else {
       return (
-        <yaopai />
+        <div></div>
       );
     }
   }
-});
+};
+
+ReactMixin.onClass(GrapherRow, Reflux.listenTo(PhotographerAlbumPoolStore, 'photographerAlbumPool'));
 
 export {GrapherRow as default};
