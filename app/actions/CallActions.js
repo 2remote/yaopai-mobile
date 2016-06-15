47d6e6e993b1b4fd2ecd35ced55b2db26813fbb4
slfbest@163.com <slfbest@163.com>
2016-06-15 09:00:28 +0800

    添加拨打电话action

---
+++ app/actions/CallActions.js
@@ -0,0 +1,16 @@
+import Reflux from 'reflux';
+import HttpFactory from '../HttpFactory';
+import API from '../api';
+
+var CallActions = Reflux.createActions({
+  'call':{children : ['success','failed']}
+});
+
+CallActions.call.listen(function(userId){
+  var data = {
+    Id: userId,
+  };
+  HttpFactory.post(API.CALL.call,data,this.success,this.failed);
+});
+
+export {CallActions as default};
