e0c05cfbfe0e001160a956c769a4150f256931bd
Haitao Yin <liubiantao@gmail.com>
2016-06-21 17:13:47 +0800

    ADD：没有作品的时候显示提示

--- app/components/common/WorkIntroGrapherList.js
+++ app/components/common/WorkIntroGrapherList.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import WorkIntroGrapherRow from './WorkIntroGrapherRow';
+import React from 'react'
+import WorkIntroGrapherRow from './WorkIntroGrapherRow'
+import NothingFound from './NothingFound.js'
 
 const WorkIntroGrapherList = ({data}) => {
-  console.table(data)
-  return (<div className="workIntroGrapherList">
-    {
-      data.map((work, i) =>
-        <WorkIntroGrapherRow
-          key={i}
-          data={work}
-        />
-      )
-    }
-  </div>
-)};
+  if (data.length > 0) {
+    return (
+      <div className="workIntroGrapherList">
+        {
+          data.map((work, i) =>
+          <WorkIntroGrapherRow
+            key={i}
+            data={work}
+            />
+          )
+        }
+      </div>
+    )
+  } else {
+    return <NothingFound title="没有找到想拍的类型？"/>
+  }
+}
 
-export default WorkIntroGrapherList;
+export default WorkIntroGrapherList
