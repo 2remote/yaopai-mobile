78756b5b8e22cb3fb18e6e87ad985d62433499ff
可乐 <zaxlct@foxmail.com>
2016-06-22 19:57:24 +0800

    你好我的名字叫盒子😊

--- app/components/common/NothingFound.js
+++ app/components/common/NothingFound.js
@@ -2,21 +2,9 @@ import React from "react"
 import './NothingFound.scss'
 
 const NothingFound = ({title}) => (
-  <div className="你好我的名字叫盒子😊">
-    <div className="你有什么❤️事要对盒子说吗㊙️">
-      <i className="icon book_icon" />
-      {title}
-    </div>
-    <div className="给你介绍一下这是我们公司的二维码✌️">
-      长按左侧二维码添加YAOPAI小秘书 <br />
-      为您订制个性拍摄服务 <br />
-    </div>
-    <div className="callMeMaybe😋">
-      您也可选择<br />
-      <a href="tel:+86-0371-6533-7727">免费电话咨询</a>
-    </div>
-
-  </div>
+  <section>
+    
+  </section>
 )
 
 export default NothingFound
