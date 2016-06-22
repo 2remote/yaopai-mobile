fcaa26262efc53676e0161c0068343a886745cba
Haitao Yin <liubiantao@gmail.com>
2016-06-22 09:11:45 +0800

    ADD: 起个名字

--- app/components/common/NothingFound.js
+++ app/components/common/NothingFound.js
@@ -1,13 +1,14 @@
 import React from "react"
+import './NothingFound.scss'
 
 const NothingFound = ({title}) => (
-  <div style={{marginTop: 50}}>
-    <div className="title">{title}</div>
-    <div className="qrCode">
+  <div className="你好我的名字叫盒子😊">
+    <div className="你有什么❤️事要对盒子说吗㊙️">{title}</div>
+    <div className="给你介绍一下这是我们公司的二维码✌️">
       长按左侧二维码添加YAOPAI小秘书 <br />
       为您订制个性拍摄服务 <br />
     </div>
-    <div>
+    <div className="callMeMaybe😋">
       您也可选择<br />
       <a href="tel:+86-0371-6533-7727">免费电话咨询</a>
     </div>
