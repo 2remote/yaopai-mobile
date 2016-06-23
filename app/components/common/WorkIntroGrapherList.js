9feec43420e58f9d2edf2c169ded63b32c45773e
Haitao Yin <liubiantao@gmail.com>
2016-06-23 19:55:01 +0800

    FIX: 没有找到想拍的类型

--- app/components/common/WorkIntroGrapherList.js
+++ app/components/common/WorkIntroGrapherList.js
@@ -3,21 +3,25 @@ import WorkIntroGrapherRow from './WorkIntroGrapherRow'
 import NothingFound from './NothingFound.js'
 
 const WorkIntroGrapherList = ({ data, searchKey = "", selectedTags = [] }) => {
-  if (data.length > 0 || ( !searchKey && selectedTags[0] )) {
+  if (data.length > 0) {
     return (
       <div className="workIntroGrapherList">
         {
           data.map((work, i) =>
-          <WorkIntroGrapherRow
-            key={i}
-            data={work}
+            <WorkIntroGrapherRow
+              key={i}
+              data={work}
             />
           )
         }
       </div>
     )
-  } else {
+  } else if ( searchKey || selectedTags[0] ) {
     return <NothingFound title="没有找到想拍的类型？"/>
+  } else {
+    return (
+      <div />
+    )
   }
 }
 
