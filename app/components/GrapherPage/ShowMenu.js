f67960ba76b2adc8c85f04c8d91dd437919335fc
Haitao Yin <liubiantao@gmail.com>
2016-06-24 18:05:36 +0800

    ADD: 摄影师搜索

---
+++ app/components/GrapherPage/ShowMenu.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import $ from 'jquery'
+
+const ShowMenu = (args) => {
+  const {onSearch, reset, searchKey} = args
+  const handleClick = () => $("#tagMenu").toggleClass('slide-toggle');
+  const plzResetAllOfThem = (reset) => {
+    // 清空搜索框，标签，以及重置 state
+    searchText.value = ''
+    reset()
+  }
+  const searchReadyGo = () => {
+    let text = searchText.value.trim();
+    text && onSearch(text)
+  }
+  let searchText;
+  return (
+    <section className="tagBox">
+      <div className="tagLogo icon yaopainew" />
+      <div className="tagBtn" onClick={handleClick}>
+        搜索 <i className="icon down" />
+      </div>
+
+      <div className="tagMenu" id="tagMenu">
+        <section className="input-group-dark">
+          <input
+            className="input input-block search"
+            ref={node => searchText = node}
+            type="text"
+            placeholder={searchKey || "搜索 摄影师昵称"}
+            onChange={searchReadyGo}
+          />
+        <div onClick={searchReadyGo}>
+          <span className="icon search search-icon icon-right"></span>
+          </div>
+        </section>
+
+        <div className="tagButton">
+          <button className="plzResetAllOfThem" onClick={() => plzResetAllOfThem(reset)}>重置</button>
+          {/*确定实际上就是隐藏*/}
+          <button className="yesImPretySure" onClick={handleClick}>确定</button>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ShowMenu
--- app/components/GrapherPage/index.js
+++ app/components/GrapherPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Reflux from 'reflux'
+import { History } from 'react-router'
 import $ from 'jquery'
 import DocumentTitle from 'react-document-title'
 import SidePage from '../UI/SidePage'
@@ -12,10 +13,10 @@ import './GrapherPage.scss'
 import _ from 'underscore'
 import WechatShare from '../Weixin/WechatShare'
 import Toaster from '../Toast'
-import ShowMenu from '../WorkPage/ShowMenu'
+import ShowMenu from './ShowMenu'
 
 const GrapherPage = React.createClass({
-  mixins : [Reflux.listenTo(PhotographerStore,'_onPhotographerStoreChange'), AutoLoadPageMixin],
+  mixins : [Reflux.listenTo(PhotographerStore,'_onPhotographerStoreChange'), AutoLoadPageMixin, History],
   getInitialState() {
     return {
       graphers: [],
@@ -23,22 +24,46 @@ const GrapherPage = React.createClass({
       pageCount: 0,
       total: 0,
       searchKey: '',
-      tags: [],
-      selectedTags: []
     }
   },
   componentDidMount() {
     //this.handleLoadGraphers(this.props.url);
     PhotographerActions.list()
-
+  },
+  handleUpdateSearch(key) {
+    console.warn(key);
+    this.setState({searchKey: key}, () => {
+      // 读取search过滤的数据
+      PhotographerActions.query(this.state.searchKey)
+      // 把搜索和筛选结果写入路由
+      // this.history.pushState(null, , {q: key})
+    })
+  },
+  reset() {
+    // 重置 state 和接口
+    this.setState({searchKey: ""})
+    PhotographerActions.query()
+    this.history.pushState(null)
   },
   _onPhotographerStoreChange(data) {
+    console.table(data.photographers)
     if(data.flag == 'list'){
       if(data.hintMessage){
         console.log(data.hintMessage);
       }else{
         this.setState({
-          graphers: this.state.graphers.concat(_.shuffle(data.photographers)),
+          graphers: this.state.graphers.concat(data.photographers),
+          pageCount: data.pageCount
+        });
+        this.onHideToast()
+      }
+    }
+    if(data.flag == 'photographer-query') {
+      if(data.hintMessage){
+        console.log(data.hintMessage);
+      }else{
+        this.setState({
+          graphers: data.photographers,
           pageCount: data.pageCount
         });
         this.onHideToast()
@@ -47,25 +72,20 @@ const GrapherPage = React.createClass({
   },
   onChangePage(pageIndex) {
     this.onShowToast('努力加载中...')
-    PhotographerActions.list(pageIndex)
+    PhotographerActions.list(pageIndex, 10, null, 3, this.state.searchKey)
   },
   render() {
 
-    const tagType = this.state.tags.map( x => x.Tags )
-
-    const { searchKey, selectedTags } = this.state
+    const { searchKey } = this.state
 
     return (
       <DocumentTitle title={TITLE.grapherPage}>
         <div className="grapherPage">
           <SidePage />
           <ShowMenu
-            tagType = {tagType}
-            onSelectedTag = {this.handleUpdateTags}
             onSearch = {this.handleUpdateSearch}
             reset = {this.reset}
             searchKey = {searchKey}
-            selectedTags = {selectedTags}
           />
           <GrapherList data={this.state.graphers} />
           <WechatShare title={TITLE.grapherPage} desc={TITLE.indexPage} />
