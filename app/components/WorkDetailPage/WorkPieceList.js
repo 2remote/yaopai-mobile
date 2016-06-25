c402c06edfda01a21e036bd9fb71e32565fc0a80
可乐 <zaxlct@foxmail.com>
2016-06-25 16:11:37 +0800

    @liubiantao 抽空看下

--- app/components/WorkDetailPage/WorkPieceList.js
+++ app/components/WorkDetailPage/WorkPieceList.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import {imgModifier} from '../Tools';
 
-import WorkPieceRow from './WorkPieceRow';
+const WorkPieceList = ({workPieces}) => {
+  // console.log(typeof workPieces) // object
+  const workNodesHack = Array.prototype.slice.call(workPieces)
+  // 如果不把这个数组强制在转换成数组, 如此调用 workPieces.map() 会报错： workPieces.map is not a function
+  const workNodes = workNodesHack.map((photo, i) =>
+    <section key={i} style={{lineHeight: 0}} className="workPieceRow">
+      <img
+        style={{width:'100%'}}
+        src={imgModifier(photo.Url, "work")}
+      />
+    </section>
+  );
 
-var WorkPieceList = React.createClass({
-  getDefaultProps: function() {
-    return {
-      workPieces: []
-    };
-  },
-  render: function() {
-    var workNodes = this.props.workPieces.map(function(img, i){
-      return (<WorkPieceRow data={img} key={i} />);
-    });
+  return (
+    <div className="workPieceList">
+      <hr className="moreWork"/>
+      <span className="des">客片展示</span>
+      {workNodes}
+    </div>
+  );
+}
 
-    return (
-      <div className="workPieceList">
-        <hr className="moreWork"/>
-        <span className="des">客片展示</span>
-        {workNodes}
-      </div>
-    );
-  }
-});
 export {WorkPieceList as default};
