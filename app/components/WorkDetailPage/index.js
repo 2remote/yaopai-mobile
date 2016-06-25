aec477c40889149e5e62f3167bb2ebb2e341cc3c
可乐 <zaxlct@foxmail.com>
2016-06-25 16:06:24 +0800

    createClas =>  Class

--- app/components/WorkDetailPage/index.js
+++ app/components/WorkDetailPage/index.js
@@ -1,53 +1,72 @@
 import React from 'react';
-import Reflux from 'reflux';
+import Reflux from 'reflux'
+import ReactMixin from 'react-mixin'
 import DocumentTitle from 'react-document-title';
 
 import WorkTitle from './WorkTitle';
 import WorkPieceList from './WorkPieceList';
-import GrapherAvatar from '../common/GrapherAvatar';
 import ActionBar from './ActionBar';
 import WorkDetail from './WorkDetail';
 import AboutGrapher from './AboutGrapher';
-import AboutYAOPAI from './AboutYAOPAI';
+import AboutYAOPAILayout from './AboutYAOPAILayout';
 import SidePage from '../UI/SidePage';
 
-import UserActions from '../../actions/UserActions';
 import AlbumsStore from '../../stores/AlbumsStore';
 import AlbumsActions from '../../actions/AlbumsActions';
 import { GET_WORK_DETAIL, TITLE } from '../Tools';
 import {History} from 'react-router'
-import Share from '../Share';
 import './index.scss';
 import WechatShare from '../Weixin/WechatShare';
 
-var WorkDetailPage = React.createClass({
-  mixins : [Reflux.listenTo(AlbumsStore,'_onAlbumsStoreChange'),History],
-  getInitialState: function() {
-    return {
-      data: {
-        Photographer: {},
-        Detail:{},
-      }
-    };
-  },
-  showTop: function(){
+class WorkDetailPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photographer: {},
+      detail: {},
+      photos: {},
+      workData: {
+        title: '',
+        price: '',
+        cover: '',
+        description: '',
+        id: '',
+      },
+    }
+  }
+
+  showTop(){
     window.scrollTo(0,0);
-  },
-  componentDidMount: function() {
-    if(!this.props.params.Id)
-      return this.history.pushState(null,'/work');
+  }
+
+  componentDidMount() {
+    if(!this.props.params.Id) return this.history.pushState(null,'/work');
     AlbumsActions.get(this.props.params.Id);
-  },
-  _onAlbumsStoreChange :function(data){
+  }
+
+  _onAlbumsStoreChange(data){
     if(data.flag == 'get'){
       if(data.hintMessage){
         console.log(data.hintMessage);
       }else{
-        this.setState({data : data.workData});
+        console.log(data)
+        this.setState({
+          photographer : data.workData.Photographer,
+          detail : data.workData.Detail,
+          photos : data.workData.Photos,
+          workData: {
+            title: data.workData.Title,
+            price: data.workData.Price,
+            cover: data.workData.Cover,
+            description: data.workData.Description,
+            id: data.workData.Id,
+          }
+        });
       }
     }
-  },
-  renderPrice: function(){
+  }
+
+  renderPrice(){
     const negotiable = this.state.data.Negotiable;
     const price = this.state.data.Price;
     let priceResult = "面议";
@@ -61,24 +80,26 @@ var WorkDetailPage = React.createClass({
       ref="workPrice"
       className="workPrice">{priceResult}</span>
     )
-  },
-  render: function() {
-    console.table(this.state.data);
+  }
+
+  render() {
     return (
       <div className="workDetailPage" onload={this.showTop()}>
-        <DocumentTitle title={this.state.data.Title + TITLE.workDetailPage} />
+        <DocumentTitle title={this.state.workData.title + TITLE.workDetailPage} />
         <SidePage />
-        <WorkTitle data={this.state.data} />
-        <WorkDetail data={this.state.data} />
-        <AboutGrapher data={this.state.data} />
-        <WorkPieceList workPieces={this.state.data.Photos} />
-        <ActionBar data={this.state.data} />
-        <AboutYAOPAI />
-        <WechatShare title={this.state.data.Title} desc={this.state.data.Description} imgUrl={this.state.data.Cover}>
-        </WechatShare>
+        <WorkTitle data={this.state.workData} />
+        <WorkDetail data={this.state.detail} />
+        <AboutGrapher data={this.state.photographer} id={this.state.workData.id} />
+        <WorkPieceList workPieces={this.state.photos} />
+        <ActionBar id={this.state.workData.id} price={this.state.workData.price} />
+        <AboutYAOPAILayout />
+        <WechatShare title={this.state.workData.title} desc={this.state.workData.description} imgUrl={this.state.workData.cover} />
       </div>
     );
   }
-});
+}
+
+ReactMixin.onClass(WorkDetailPage, Reflux.listenTo(AlbumsStore, '_onAlbumsStoreChange'));
+ReactMixin.onClass(WorkDetailPage, History);
 
 export {WorkDetailPage as default};
