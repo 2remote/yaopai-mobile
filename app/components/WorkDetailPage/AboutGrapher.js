03ebce0e944d0dfb903979e77b545ae0c4892a60
可乐 <zaxlct@foxmail.com>
2016-06-25 16:32:15 +0800

    重构作品详情页 (#381)
    * iPhone 5 样式错乱
    
    * 作品详情页 iPhone 适配
    
    * fix #378
    
    * ES5 => 函数式组件
    
    * 删除废弃文件
    
    * rename （纯布局组件命名请用 Layout结尾）
    
    @liubiantao @stonemakers
    
    * createClas =>  Class
    
    * @liubiantao 抽空看下
    
    * 更改注释

--- app/components/WorkDetailPage/AboutGrapher.js
+++ app/components/WorkDetailPage/AboutGrapher.js
@@ -1,36 +1,21 @@
 import React from 'react';
-import { Router, Route, Link } from 'react-router';
+import { Link } from 'react-router';
 
-
-var AboutGrapher = React.createClass({
-  getDefaultProps: function() {
-    return {
-      data: {
-        Photographer: {
-          Avatar: 'https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3156704277,4221589279&fm=96',
-        }
-      }
-    };
-  },
-  render: function() {
-    console.log('ag', this.props.data);
-    return (
-        <div className="grapher-panel">
-          <Link to={"/grapherDetail/"+this.props.data.UserId} >
-            <div className="avatar" style={
-            {backgroundImage:`url(${this.props.data.Photographer.Avatar})`,
-            backgroundSize:'contain',
-            height:'80px',
-            width:'80px',
-            borderRadius:'50%'}
-            }>
-            </div>
-          </Link>
-          <p className="uName">{this.props.data.Photographer.NickName}</p>
-          <p className="uDes">YAOPAI认证摄影师</p>
-        </div>
-    );
-  }
-});
+const AboutGrapher =({data, id}) => (
+  <div className="grapher-panel">
+    <Link to={`/grapherDetail/${id}`}>
+      <div className="avatar" style={
+      {backgroundImage:`url(${data.Avatar})`,
+      backgroundSize:'contain',
+      height:'80px',
+      width:'80px',
+      borderRadius:'50%'}
+      }>
+      </div>
+    </Link>
+    <p className="uName">{data.NickName}</p>
+    <p className="uDes">YAOPAI认证摄影师</p>
+  </div>
+)
 
 export {AboutGrapher as default};
--- app/components/WorkDetailPage/WorkTitle.js
+++ app/components/WorkDetailPage/WorkTitle.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const WorkTitle = ({data}) => (
   <section className="workTitlePanel">
-    <div className="cover" style={{backgroundImage:`url(${data.Cover})`,backgroundSize:'cover'}}/>
+    <div className="cover" style={{backgroundImage:`url(${data.cover})`,backgroundSize:'cover'}}/>
     <p className="title-a">
-      <span className="title">{data.Title}</span>
-      <span className="price">￥{data.Price}</span>
+      <span className="title">{data.title}</span>
+      <span className="price">￥{data.price}</span>
     </p>
-    <p className="sub-title">{ data.Description}</p>
+    <p className="sub-title">{ data.description}</p>
   </section>
 )
 
