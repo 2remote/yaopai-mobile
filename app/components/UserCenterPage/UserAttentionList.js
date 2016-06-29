b779cff7770f05d8249eeb5d1b95d9b41088c88b
可乐 <zaxlct@foxmail.com>
2016-06-29 16:03:51 +0800

    😂我来破坏阵型

--- app/components/UserCenterPage/UserAttentionList.js
+++ app/components/UserCenterPage/UserAttentionList.js
@@ -5,7 +5,7 @@ import { RouteTransition, presets } from 'react-router-transition'
 class UserAttentionList extends React.Component{
   render() {
     return(
-      <section className="collect-list">
+      <section className="attention-list">
         <UserMarkLayout />
         <RouteTransition { ...presets.slideRight } pathname="/center/mark/user_attention">
           collect
