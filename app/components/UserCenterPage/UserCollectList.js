2d395e2d3df6114668e6e8f7e29fc34ca82cd38c
可乐 <zaxlct@foxmail.com>
2016-06-29 17:01:09 +0800

    拿到收藏数据

--- app/components/UserCenterPage/UserCollectList.js
+++ app/components/UserCenterPage/UserCollectList.js
@@ -2,8 +2,34 @@ import React from 'react';
 import UserMarkLayout from './UserMarkLayout';
 import { RouteTransition, presets } from 'react-router-transition'
 
+import Reflux from 'reflux';
+import ReactMixin from 'react-mixin';
+import AlbumsActions from '../../actions/AlbumsActions';
+import AlbumsStore from '../../stores/AlbumsStore';
+
 class UserCollectList extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      works: [],
+    }
+  }
+
+  componentWillMount() {
+    // 蛋疼的传参
+    // 查询当前用户已关注作品
+    AlbumsActions.search(null, "", null, null, null, true);
+  }
+
+  onSearchSuccess(data) {
+    console.log(data)
+    this.setState({
+      works: data.workList,
+    })
+  }
+
   render() {
+    
     return(
       <section className="collect-list">
         <UserMarkLayout />
@@ -15,4 +41,5 @@ class UserCollectList extends React.Component{
   }
 }
 
+ReactMixin.onClass(UserCollectList, Reflux.listenTo(AlbumsStore, 'onSearchSuccess'));
 export default UserCollectList
