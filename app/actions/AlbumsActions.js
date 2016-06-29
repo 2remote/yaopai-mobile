df4df3496c34935ca08a733171d973566227efc5
可乐 <zaxlct@foxmail.com>
2016-06-29 17:24:15 +0800

    接入数据

--- app/actions/AlbumsActions.js
+++ app/actions/AlbumsActions.js
@@ -56,7 +56,7 @@ function searchQuery(tags = null, key = "", pageIndex = 1, pageSize = 10, UserId
     Mark,
     Fields : 'Id,Title,UserId,Description,Service,Price,MarkExist,' +
     'Cover,Cut,Photos.Id,Photos.AlbumsId,Photos.Url,Photos.Description,' +
-    'User.Id,Photographer.NickName,Photographer.Avatar,Views,Price,' +
+    'User.Id,Photographer.NickName,Photographer.CityName,Photographer.Avatar,Views,Price,' +
     'Detail.Duration,Detail.PlateCount,Detail.TruingCount,Detail.CostumeCount,' +
     'Detail.MakeUpSupport,Detail.OriginalSupport,Detail.PhysicalSupport,Detail.PhysicalDetail,' +
     'Detail.UnitCount,Detail.SceneCount,Detail.PeopleCount,Detail.SeatCount,Detail.PlaceType',
--- app/components/UserCenterPage/UserCollectList.js
+++ app/components/UserCenterPage/UserCollectList.js
@@ -22,19 +22,33 @@ class UserCollectList extends React.Component{
   }
 
   onSearchSuccess(data) {
-    console.log(data)
     this.setState({
       works: data.workList,
     })
   }
 
   render() {
-    
+    const CollectRow = this.state.works.map((data, i) => (
+      <section className="collect-row" key={i}>
+        <div className="media-hd">
+          <img src={data.Cover} />
+        </div>
+        <div className="media-bd">
+          <div className="media-header">
+            <p className="media-title">{data.Title}</p>
+            <span>{data.Photographer.CityName}</span>
+          </div>
+          <p className="media-desc">{data.Description}</p>
+          <p className="media-price">{data.Prace}</p>
+        </div>
+      </section>
+    ))
+
     return(
       <section className="collect-list">
         <UserMarkLayout />
         <RouteTransition { ...presets.slideLeft } pathname="/center/mark/user_collect">
-          collect
+          {CollectRow}
         </RouteTransition>
       </section>
     )
