f54047e948b908ad6da626b1529c8a847a1522b6
可乐 <zaxlct@foxmail.com>
2016-07-01 13:33:47 +0800

    fix #406

--- app/components/UserCenterPage/UserCollectList.js
+++ app/components/UserCenterPage/UserCollectList.js
@@ -12,7 +12,7 @@ class UserCollectList extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      works: [],
+      CollectRow: <div></div>,
     }
   }
 
@@ -23,47 +23,46 @@ class UserCollectList extends React.Component{
   }
 
   onSearchSuccess(data) {
-    this.setState({
-      works: data.workList,
-    })
+    if(!data.workList.length) {
+      this.setState({
+        CollectRow: <div className="nothing-found">您还没有收藏过作品：）</div>
+      })
+    } else {
+      this.setState({
+        CollectRow: data.workList.map((data, i) => (
+          <section
+            className="collect-row"
+            key={i}
+            onClick={() => this.props.history.replaceState(null,`workDetail/${data.Id}`)}
+          >
+            <div className="media-hd">
+              <span
+                style={{background: `url(${data.Cover}) no-repeat`, backgroundSize: 'cover'}}
+                className="media-work"
+              />
+            </div>
+            <div className="media-bd">
+              <div className="media-header">
+                <p className="media-title fl">{data.Title}</p>
+                <span className="media-city fr">
+                  {data.Photographer.CityName.substring(0, data.Photographer.CityName.length - 1)}
+                </span>
+              </div>
+              <p className="media-desc">{data.Description}</p>
+              <p className="media-price fr">￥{data.Price}/人</p>
+            </div>
+          </section>
+        ))
+      });
+    }
   }
 
   render() {
-    let CollectRow = <div></div>
-    if (!this.state.works.length) {
-      CollectRow = <div className="nothing-found">你还没有收藏过作品：）</div>
-    } else {
-      CollectRow = this.state.works.map((data, i) => (
-        <section
-          className="collect-row"
-          key={i}
-          onClick={() => this.props.history.replaceState(null,`workDetail/${data.Id}`)}
-        >
-          <div className="media-hd">
-            <span
-              style={{background: `url(${data.Cover}) no-repeat`, backgroundSize: 'cover'}}
-              className="media-work"
-            />
-          </div>
-          <div className="media-bd">
-            <div className="media-header">
-              <p className="media-title fl">{data.Title}</p>
-              <span className="media-city fr">
-                {data.Photographer.CityName.substring(0, data.Photographer.CityName.length - 1)}
-              </span>
-            </div>
-            <p className="media-desc">{data.Description}</p>
-            <p className="media-price fr">￥{data.Price}/人</p>
-          </div>
-        </section>
-      ))
-    }
-
     return(
       <section className="collect-list">
         <UserMarkLayout />
         <RouteTransition { ...presets.slideLeft } pathname="/center/mark/user_collect">
-          {CollectRow}
+          {this.state.CollectRow}
         </RouteTransition>
       </section>
     )
