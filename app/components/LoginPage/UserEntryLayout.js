cc7806745697711d35606db81bc7375a04e351b6
可乐 <zaxlct@foxmail.com>
2016-07-13 09:06:17 +0800

    背景图适配不同 iPhone 手机

--- app/components/LoginPage/UserEntryLayout.js
+++ app/components/LoginPage/UserEntryLayout.js
@@ -2,10 +2,19 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const windowHeight = document.body.scrollHeight;
+let entryCoverHeight = windowHeight;
+if (windowHeight >= 736) { // iPhone 6 Plus
+  entryCoverHeight = 0.56 * windowHeight;
+} else if (windowHeight >= 667 && windowHeight < 736) { // iPhone 6
+  entryCoverHeight = 0.52 * windowHeight;
+} else if (windowHeight >= 568 && windowHeight < 667) { // iPhone 5 || SE
+  entryCoverHeight = 0.45 * windowHeight;
+}
+
 const UserEntryLayout = () => (
-  <section className="entry-container" style={{height: windowHeight * 0.51 + 'px'}}>
+  <section className="entry-container" style={{height: entryCoverHeight + 'px'}}>
     <header className="entry-header">
-      <i className="icon yaopainew"></i>
+      <i className="icon yaopainew"></i><br/>
       <p>全球&nbsp;预约&nbsp;摄影师&nbsp;平台</p>
     </header>
     <nav className="entry-nav">
