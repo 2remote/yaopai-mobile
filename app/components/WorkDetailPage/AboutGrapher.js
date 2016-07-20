9e554aa7a152133ac20c387ea025b0021abb9714
可乐 <zaxlct@foxmail.com>
2016-07-20 10:38:08 +0800

    @liubiantao

--- app/components/WorkDetailPage/AboutGrapher.js
+++ app/components/WorkDetailPage/AboutGrapher.js
@@ -15,7 +15,7 @@ const AboutGrapher =({data, id}) => (
     </Link>
     <p className="uName">{data.NickName}</p>
     <p className="uDes">YAOPAI认证摄影师</p>
-    <div className="button btn-dark download-app">
+    <div className="button btn-dark download-app" onClick={() => {/**/}>
       <i className="icon talk" /> 下载 APP 联系我
     </div>
   </div>
