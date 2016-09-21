1020c848f72322f07abe928b1cfa14c45c73bf0f
可乐 <zaxlct@foxmail.com>
2016-09-21 19:11:30 +0800

    调整引导图片和邮箱注册提示 (#586)
    * Revert "去掉首页引导动画"
    
    This reverts commit 8a41ef5efbe71c6b55193b2c1039d4d5456a752e.
    
    * 更换首页引导图片URL
    
    * 去掉多余的邮箱提示功能

---
+++ app/components/WorkPage/AnimationGuide.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import $ from 'jquery'
+
+class AnimationGuide extends React.Component {
+  componentDidMount() {
+    if (localStorage.getItem('isFirstEnter') === 'true') {
+      $('#animation-guide').hide()
+      return
+    }
+    $(() => {
+      $('#guide-1').fadeIn('slow')
+
+      $('#animation-guide').on('click', () => {
+        $('#guide-1').fadeOut('300')
+        $('#guide-2').fadeIn('slow', () => {
+          $('#animation-guide').unbind('click')
+
+          $('#animation-guide').on('click', () => {
+
+            $('#guide-2').fadeOut('300')
+            $('#guide-3').fadeIn('slow', () => {
+              $('#animation-guide').unbind('click')
+              $('#animation-guide').click(() => $('#animation-guide').hide(300))
+            })
+          })
+        })
+      })
+
+      localStorage.setItem('isFirstEnter', 'true')
+    })
+  }
+
+  render() {
+    return(
+      <section className="animation-guide" id="animation-guide">
+        <img
+          src="http://oawjlomvj.bkt.clouddn.com/cat-1.png"
+          id="guide-1"
+        />
+        <img
+          src="http://oawjlomvj.bkt.clouddn.com/cat-2.png"
+          id="guide-2"
+
+        />
+        <img
+          src="http://oawjlomvj.bkt.clouddn.com/cat-3.png"
+          id="guide-3"
+        />
+      </section>
+    )
+  }
+}
+
+export default AnimationGuide
