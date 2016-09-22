14e5ab42ee831f963fc3ddc627f741b3f57e7580
zaxlct <zaxlct@foxmail.com>
2016-09-22 16:32:04 +0800

    改称函数式组件

--- app/components/layout/center/grapher/order/OrderTabLayout.js
+++ app/components/layout/center/grapher/order/OrderTabLayout.js
@@ -3,9 +3,9 @@ import OrderListLayout from './OrderListLayout'
 import WeuiNavbar from '../../../../UI/WeuiNavbar'
 import { OrderStatus } from '../../../../Tools'
 import OrderActions from '../../../../../actions/OrderActions'
-import $ from 'jquery'
+import OrderSearchBar from './OrderSearchBar'
 
-let navList = [
+const navList = [
   {
     filterType: OrderStatus.ALL,
     text: '全部订单'
@@ -23,59 +23,18 @@ let navList = [
     text: '已关闭'
   }]
 
-class OrderTabLayout extends React.Component {
-  /**
-   * 我也不知道为什么要把整个data传出来
-   * @param data
-   */
-  getIndex(data) {
-    OrderActions.type(data.filterType)
-  }
+const OrderTabLayout = ({location}) => {
+  const getIndex = data => OrderActions.type(data.filterType)
 
-  onSearchFocus() {
-    $('#search_bar').addClass('weui_search_focusing')
-  }
+  const getSearchText = selectText => OrderActions.getSearchText(selectText)
 
-  clearText() {
-    $('#search_input').val('')
-  }
-
-  onSearchSubmit = e => {
-    e.preventDefault()
-    $('#search_bar').removeClass('weui_search_focusing')
-    var text = $('#search_input').val().trim()
-  }
-  render() {
-    return (
-      <div className="weui_tab">
-        <WeuiNavbar list={navList} onClick={this.getIndex.bind(this)} />
-        <div className="weui_search_bar" id="search_bar" style={{position: 'absolute', width: '100%', top: '48'}}>
-          <form className="weui_search_outer" onSubmit={this.onSearchSubmit}>
-            <div className="weui_search_inner">
-              <i className="weui_icon_search" />
-              <input
-                type="search"
-                className="weui_search_input"
-                id="search_input"
-                placeholder="搜索"
-                onFocus={this.onSearchFocus}
-                required
-              />
-            <a href="javascript:" onClick={this.clearText} className="weui_icon_clear" id="search_clear" />
-            </div>
-            <label htmlFor="search_input" className="weui_search_text" id="search_text">
-              <i className="weui_icon_search" />
-              <span>搜索</span>
-            </label>
-          </form>
-          <a href="javascript:" onClick={this.onSearchBlur} className="weui_search_cancel" id="search_cancel">取消 </a>
-        </div>
-        <OrderListLayout location={{ pathname: this.props.location.pathname }} />
-      </div>
-    )
-  }
+  return (
+    <div className="weui_tab">
+      <WeuiNavbar list={navList} onClick={getIndex} />
+      <OrderSearchBar onSearch={getSearchText} />
+      <OrderListLayout location={{ pathname: location.pathname }} />
+    </div>
+  )
 }
 
 export default OrderTabLayout
-
-      // <!--<a href="javascript:;" class="weui_btn weui_btn_primary">点击展现searchBar</a>-->
