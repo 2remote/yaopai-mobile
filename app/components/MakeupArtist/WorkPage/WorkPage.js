d4cf49984875bb0041fba5b17f3edcfb0802f1fc
zaxlct <zaxlct@foxmail.com>
2016-10-10 19:10:53 +0800

    删除无用括号

--- app/components/MakeupArtist/WorkPage/WorkPage.js
+++ app/components/MakeupArtist/WorkPage/WorkPage.js
@@ -62,7 +62,7 @@ class WorkPage extends React.Component {
         <CharacterBar />
         <CharacterSelect data={this.state.tagListResult} />
         <Toaster ref="toast" isWorkPage={true} bottom={true} duration="1000000"/>
-        {<WorkIntroGrapherList data={this.state.result} />}
+        <WorkIntroGrapherList data={this.state.result} />
       </div>
     )
   }
