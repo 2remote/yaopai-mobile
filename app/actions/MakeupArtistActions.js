2083fd010ee6829293a37ceda3d3c54689279b61
zaxlct <zaxlct@foxmail.com>
2016-10-25 21:30:30 +0800

    重构范例

--- app/actions/MakeupArtistActions.js
+++ app/actions/MakeupArtistActions.js
@@ -3,17 +3,16 @@ import HttpFactory from '../HttpFactory'
 import API from '../api'
 
 const MakeupArtistActions = Reflux.createActions({
-  'getInfo' : {children : ['success','failed']},
+  getInfo: {asyncResult: true},
 })
-/*
-  得到指定id的摄影师信息
-*/
+
+// 得到指定id的摄影师信息
 MakeupArtistActions.getInfo.listen(function(id){
   let data = {
     Id : id,
     Fields : 'Id,Sales,TotalAlbums,Views,Marks,MarkExist,CityName,NickName,Avatar,Signature,Tags.Name',
   }
-  HttpFactory.post(API.MakeupArtist.getInfo,data,this.success,this.failed)
+  HttpFactory.post(API.MakeupArtist.getInfo, data, this.completed, this.failed)
 })
 
 export default MakeupArtistActions
