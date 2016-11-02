bc16138abdba70beeb0b4708e84ee5e83b4840f3
slfbest@163.com <slfbest@163.com>
2016-11-03 01:52:11 +0800

    添加活动报名页面

---
+++ app/components/ActivityPage/ActivityJoin/ActivityJoin.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Reflux from 'reflux'
+import ReactMixin from 'react-mixin'
+
+import ActivityJoinLayout from './ActivityJoinLayout'
+
+import ActivityStore from '../../../stores/ActivityStore'
+
+class ActivityJoin extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      display : false,
+      id : this.props.source.Id,
+      result : {}
+    }
+  }
+
+  hideJoinPage() {
+    this.props.hideJoinPage()
+  }
+
+  // 回调报名状态
+  getActivityJoin(data) {
+    if(data.flag === 'join'){
+      console.log(data)
+      if(data.add.isAdd){
+        alert("报名成功！")
+      }else{
+        alert(data.hintMessage)
+        return
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ActivityJoinLayout hideJoinPage={this.hideJoinPage.bind(this)} source={this.state}  />
+      </div>
+    )
+  }
+}
+
+ReactMixin.onClass(ActivityJoin,Reflux.listenTo(ActivityStore, 'getActivityJoin'))
+export default ActivityJoin
---
+++ app/components/ActivityPage/ActivityJoin/ActivityJoinLayout.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import $ from 'jquery'
+
+import ActivityActions from '../../../actions/ActivityActions'
+
+import { Link } from 'react-router'
+
+class ActivityJoinLayout extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      name : '',
+      tel : '',
+      gender : 1,
+      remark : ''
+    }
+  }
+
+  // 修改姓名
+  _handleChangeName(e) {
+    this.setState({
+      name : e.target.value,
+    })
+  }
+
+  // 修改电话
+  _handleChangeTel(e) {
+    this.setState({
+      tel : e.target.value,
+    })
+  }
+
+  // 修改备注
+  _handleChangeRemark(e) {
+    this.setState({
+      remark : e.target.value,
+    })
+  }
+
+  // 修改性别女
+  _handleChangeW(e) {
+    this.setState({
+      gender : 0,
+    })
+  }
+
+  // 修改性别男
+  _handleChangeM(e) {
+    this.setState({
+      gender : 1,
+    })
+  }
+
+  _handleSubmit() {
+    let data = this.state
+    data.id = this.props.source.id
+    ActivityActions.add(data)
+  }
+
+  hide(e) {
+    this.props.hideJoinPage()
+  }
+
+  render() {
+    return (
+      <div className="ac-masks" >
+        <div className="ac-join">
+          <h1>YAOPAI活动报名</h1>
+          <div className="ac-inputGroup">
+            <input className="ac-input" type="text" onChange={this._handleChangeName.bind(this)} placeholder="填写姓名" />
+          </div>
+          <div className="ac-inputGroup">
+            <input className="ac-input" type="tel" onChange={this._handleChangeTel.bind(this)} placeholder="填写手机号码" />
+          </div>
+          <div className="ac-inputGroup">
+            <p>性别</p>
+            <input className="ac-radio" name="gender" defaultChecked onClick={this._handleChangeM.bind(this)} type="radio" />&nbsp;&nbsp;男&nbsp;&nbsp;&nbsp;
+            <input name="gender" className="ac-radio" onClick={this._handleChangeW.bind(this)} type="radio" />&nbsp;&nbsp;女
+          </div>
+          <div className="ac-inputGroup">
+            <p>备注</p>
+            <input className="ac-input" rows="2" onChange={this._handleChangeRemark.bind(this)} cols="2" type="text-area" />
+          </div>
+          <a className="ac-submit" onClick={this._handleSubmit.bind(this)}>提交报名</a>
+        </div>
+
+        <a className="ac-close" onClick={this.hide.bind(this)}>X</a>
+      </div>
+    )
+  }
+}
+
+export default ActivityJoinLayout
