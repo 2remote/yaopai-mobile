46cea404f43fe9d3d243821ce84ff93ec91632a7
slfbest@163.com <slfbest@163.com>
2016-11-03 01:53:32 +0800

    业务层添加及修改

--- app/actions/ActivityActions.js
+++ app/actions/ActivityActions.js
@@ -2,26 +2,39 @@ import Reflux from 'reflux'
 import HttpFactory from '../HttpFactory'
 import API from '../api'
 
-var ActivityActions = Reflux.createActions({
-  'get' : {children:['success','failed']},
-  'search':{children : ['success','failed']}
+const ActivityActions = Reflux.createActions({
+  search: {children : ['success','failed']},
+  getDetail: {children : ['success','failed']},
+  add: {children : ['success','failed']}
 })
 
-ActivityActions.search.listen(function(pageIndex = 1 ,pageSize = 10){
-  var data = {
-    PageIndex:pageIndex,
-    PageSize:pageSize,
-    Fields : 'Id,Cover,Link'
+// 获取活动列表信息
+ActivityActions.search.listen(function(){
+  let data = {
+    Fields : 'Id,Cover,Deadline,Title,ProvinceName,HomeRecommended,HomeSorting,IsExternal,Link',
   }
-  HttpFactory.post(API.ACTIVITY.search,data,this.success,this.failed)
+  HttpFactory.post(API.ACTIVITY.search, data, this.success, this.failed)
 })
 
-ActivityActions.get.listen(function(Id){
-  var data ={
-    Id: Id,
-    Fields : 'Id,Title,Cover,Content'
+// 获取活动列表信息
+ActivityActions.getDetail.listen(function(id){
+  let data = {
+    Id : id,
+    Fields : 'Id,Cover,Title,SubTitle,Display,Content',
   }
-  HttpFactory.post(API.ACTIVITY.get,data,this.success,this.failed)
+  HttpFactory.post(API.ACTIVITY.get, data, this.success, this.failed)
 })
 
-export {ActivityActions as default}
+// 获取活动报名信息
+ActivityActions.add.listen(function(item){
+  let data = {
+    activityId : item.id,
+    tel : item.tel,
+    name : item.name,
+    sex : item.gender,
+    remarks : item.remark
+  }
+  HttpFactory.post(API.ACTIVITY.add, data, this.success, this.failed)
+})
+
+export default ActivityActions
